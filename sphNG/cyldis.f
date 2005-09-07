3314cf3b51fa097dbeb13057b5283e5c9086db1d
Daniel Price <daniel.price@monash.edu>
2005-09-07 15:22:32 +0000

    compiler warnings - include COMMON block for key
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@72 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/cyldis.f
+++ sphNG/cyldis.f
@@ -14,6 +14,7 @@ c************************************************************
       INCLUDE 'COMMONS/diskbd'
       INCLUDE 'COMMONS/debug'
       INCLUDE 'COMMONS/logun'
+      INCLUDE 'COMMONS/treecom_P'
       INCLUDE 'COMMONS/maspres'
       INCLUDE 'COMMONS/ptmass'
 

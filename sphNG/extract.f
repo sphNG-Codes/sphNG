abc80314cf41bce2e5162d671905de0c894c87ed
Daniel Price <daniel.price@monash.edu>
2006-05-09 08:40:25 +0000

    extract_mhd now just extract: better anyway
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@159 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/extract.f
+++ sphNG/extract.f
@@ -19,25 +19,40 @@ c************************************************************
       INCLUDE 'COMMONS/ener2'
       INCLUDE 'COMMONS/ener3'
       INCLUDE 'COMMONS/fracg'
+      INCLUDE 'COMMONS/polyk2'
       INCLUDE 'COMMONS/phase'
       INCLUDE 'COMMONS/ptmass'
       INCLUDE 'COMMONS/binary'
 c      INCLUDE 'COMMONS/torq'
       INCLUDE 'COMMONS/timei'
       INCLUDE 'COMMONS/stepopt'
+      INCLUDE 'COMMONS/sort'
+      INCLUDE 'COMMONS/tming'
+      INCLUDE 'COMMONS/numpa'
+      INCLUDE 'COMMONS/radtrans'
+      INCLUDE 'COMMONS/mhd'
+      INCLUDE 'COMMONS/gradhterms'
+      INCLUDE 'COMMONS/varmhd'
+      INCLUDE 'COMMONS/presb'
 
-      CHARACTER*20 ifile(10), ofile
-      CHARACTER*1 iok, iok2, iokm
+      CHARACTER*40 ifile(10), ofile
+      CHARACTER*1 iok, iok2, iokm, iaddmhd
       CHARACTER*100 fileident
       INTEGER*4 int1, int2, int1i, int2i, int3i
       INTEGER*8 number8
-      DIMENSION nums(8)
-
-1000  FORMAT (A20)
+      DIMENSION nums1(8),nums2(8),nums3(8),nums4(8)
+      DIMENSION Bxyz(3,imhd)
+      
+1000  FORMAT (A40)
 1001  FORMAT (A1)
 
       nfile = 1
-
+c
+c--assume that MHD variable is B
+c
+      IF (imhd.EQ.idim) THEN
+         varmhd = 'Bvol'
+      ENDIF
       PRINT *, 'name of file to be extracted ?'
 
       DO i = 1, nfile
@@ -54,6 +69,10 @@ c      INCLUDE 'COMMONS/torq'
       READ (*,1001) iok
       PRINT *, 'do you want to reset centre of mass ?'
       READ (*,1001) iokm
+      IF (imhd.EQ.idim) THEN
+         PRINT *, 'do you want to add/reset magnetic fields ?'
+         READ (*,1001) iaddmhd
+      ENDIF
 
       image = 0
       imo = 0
@@ -63,7 +82,7 @@ c      INCLUDE 'COMMONS/torq'
          OPEN (UNIT = 8, FILE = ifile(k), FORM = 'unformatted')
          PRINT *, 'reading file ', ifile(k)
 
-         DO 10 image = 1, 9999
+         DO 10 image = 1, 1
 
             PRINT *, 'reading image number ', image
 c
@@ -95,8 +114,8 @@ c--Single values
 c
 c--Default int
       READ (8, END=100) number
-      IF (number.NE.6) THEN
-         WRITE (*,*) 'ERROR 4 in rdump'
+      IF (number.LT.6) THEN
+         WRITE (*,*) 'ERROR 4 in rdump: not enough default ints'
          CALL quit
       ENDIF
       READ (8, END=100) npart,n1,n2,nreassign,naccrete,nkill
@@ -106,46 +125,97 @@ c--int*1, int*2, int*4, int*8
       END DO
 c--Default real
       READ (8, END=100) number
-      IF (number.NE.14) THEN
-         WRITE (*,*) 'ERROR 5 in rdump'
+      IF (number.LT.14) THEN
+         WRITE (*,*) 'ERROR 5 in rdump: not enough default reals'
          CALL quit
       ENDIF
-      READ (8, END=100) gt, dtmax, gamma, rhozero, RK2,
-     &     escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
-     &     specang, ptmassin
+      IF (imhd.EQ.idim .AND. number.GE.18) THEN
+         READ (8, END=100) gt, dtmaxdp, gamma, rhozero, RK2,
+     &        escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
+     &        specang, ptmassin, tmag, Bextx, Bexty, Bextz
+      ELSE
+         IF (imhd.EQ.idim) THEN
+            WRITE(*,*) 'WARNING: dump does not contain external field'
+            WRITE(*,*) '         (setting to zero)'
+            Bextx = 0.
+            Bexty = 0.
+            Bextz = 0.
+         ENDIF
+         READ (8, END=100) gt, dtmaxdp, gamma, rhozero, RK2,
+     &        escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
+     &        specang, ptmassin
+      ENDIF
 c--real*4
       READ (8, END=100) number
 c--real*8
       READ (8, END=100) number
-      IF (number.NE.3) THEN
-         WRITE (*,*) 'ERROR 6 in rdump'
+      IF (number.LT.3) THEN
+         WRITE (*,*) 'ERROR 6 in rdump: nreal8 too small header'
          CALL quit
       ENDIF
-      READ (8, END=100) udist, umass, utime
+      IF (imhd.EQ.idim) THEN
+         IF (number.GT.3) THEN
+            READ (8, END=100) udisti, umassi, utimei, umagfdi
+         ELSE
+            WRITE (*,*) 'WARNING: no mag field units in rdump'
+            READ (8, END=100) udisti, umassi, utimei
+            umagfdi = umagfd         
+         ENDIF
+      ELSE
+         READ (8, END=100) udisti, umassi, utimei
+      ENDIF
 c
 c--Arrays
 c
 c--Number of array lengths
 c
       READ (8, END=100) number
-      IF (number.NE.2) THEN
+      IF (number.LT.2 .OR. number.GT.4) THEN
          WRITE (*,*) 'ERROR 7 in rdump'
          CALL quit
       ENDIF
 c
 c--Read array type 1 header
 c
-      READ (8, END=100) number8, (nums(i), i=1,8)
+      READ (8, END=100) number8, (nums1(i), i=1,8)
       IF (number8.NE.npart) THEN
          WRITE (*,*) 'ERROR 8 in rdump: npart wrong'
          CALL quit
       ENDIF
       npart = number8
+      PRINT*,' npart = ',npart
 c
 c--Read array type 2 header
 c
-      READ (8, END=100) number8, (nums(i), i=1,8)
+      READ (8, END=100) number8, (nums2(i), i=1,8)
       nptmass = number8
+      PRINT*,' nptmasses = ',nptmass
+c
+c--Read array type 3 header
+c
+      IF (number.GE.3) THEN
+         READ (8, END=100) number8, (nums3(i), i=1,8)
+         IF (number8.GT.iradtrans .OR. number8.NE.0 .AND. 
+     &        number8.NE.npart) THEN
+            WRITE (*,*) 'ERROR 9 in rdump: iradtrans wrong ',number8,
+     &           iradtrans,npart
+            CALL quit
+         ENDIF
+         nradtrans = number8
+      ENDIF
+c
+c--Read array type 4 header
+c
+      IF (number.GE.4) THEN
+         READ (8, END=100) number8, (nums4(i), i=1,8)
+         IF (number8.GT.imhd .OR. number8.NE.1 .AND. 
+     &        number8.NE.npart) THEN
+            WRITE (*,*) 'ERROR 10 in rdump: imhd wrong ',number8,
+     &           imhd,npart
+            CALL quit
+         ENDIF
+         nmhd = number8
+      ENDIF
 c
 c--Read array type 1 arrays
 c
@@ -166,9 +236,31 @@ c--Default real
       DO j = 1, 4
          READ (8, END=100) (vxyzu(j,i), i=1, npart)
       END DO      
+c--skip unnecessary reals
+      IF (nums1(6).GT.9) THEN
+         DO j=1,nums1(6)-9
+            READ (8, END=100)
+         ENDDO
+      ENDIF    
 c--real*4
       READ (8, END=100) (rho(i), i=1, npart)
-      READ (8, END=100) (dgrav(i), i=1, npart)
+      IF (nlmax.EQ.1) THEN
+         iread = 1
+      ELSE
+         iread = 2
+         READ (8, END=100) (dgrav(i), i=1, npart)
+         IF (gt.EQ.0.0) THEN
+            DO j = 1, npart
+               dgrav(j) = 0.
+            ENDDO
+         ENDIF
+      ENDIF
+c--skip unnecessary real*4's
+      IF (nums1(7).GT.iread) THEN
+         DO j=1,nums1(7)-iread
+            READ (8, END=100)
+         ENDDO
+      ENDIF
 c     READ (8, END=100) (alphaMM(i), i=1, npart)
 c--real*8
 
@@ -198,6 +290,102 @@ c--Default real
 c--real*4
 
 c--real*8
+
+      IF (number.GE.3 .AND. nradtrans.EQ.npart 
+     &    .AND. encal.EQ.'r') THEN
+c
+c--Array length 3 arrays
+c      
+c--Default int
+
+c--int*1
+
+c--int*2
+
+c--int*4
+
+c--int*8
+
+c--Default real
+         DO j = 1, 5
+            READ (8, END=100) (ekcle(j,i), i=1, npart)
+         END DO
+c--real*4
+
+c--real*8
+
+      ENDIF
+      IF (number.GE.4 .AND. nmhd.EQ.npart .AND. imhd.EQ.idim) THEN
+          PRINT *,' dump file contains magnetic fields...' 
+c
+c--Array length 4 arrays
+c      
+c--Default int
+
+c--int*1
+
+c--int*2
+
+c--int*4
+
+c--int*8
+
+c--Default real
+c
+c--read B field from dump
+c
+         IF (nums4(6).LT.3) THEN
+            WRITE(*,*) 'ERROR: no MHD variables in dump file'
+            CALL quit
+         ENDIF
+         DO j = 1, 3
+            READ (8, END=100) (Bxyz(j,i), i=1, npart)
+         END DO
+c
+c--read Euler potentials from dump if necessary
+c
+         IF (varmhd.EQ.'eulr') THEN
+            IF (nums4(6).GE.5) THEN
+               DO j = 1, 2
+                  READ (8, END=100) (Bevolxyz(j,i), i=1, npart)
+               END DO
+            ELSE
+               WRITE(*,*) 'ERROR: Cannot start Euler potentials run '
+               WRITE(*,*) '       from non-Euler potentials dump'
+               CALL quit
+            ENDIF
+         ELSEIF (varmhd.EQ.'Brho') THEN
+c
+c--convert from B to B/rho for evolution
+c
+            DO i=1,npart
+               IF (rho(i).LE.0.) THEN
+                  WRITE(*,*) 'ERROR: rho <= 0 in rdump, evolving B/rho'
+                  CALL quit
+               ENDIF
+               DO j = 1,3
+                  Bevolxyz(j,i) = Bxyz(j,i)/rho(i)
+               ENDDO
+            ENDDO
+         ELSEIF (varmhd.EQ.'Bvol') THEN
+c
+c--Bevol = B if evolving B
+c
+            DO i=1,npart
+               DO j = 1,3
+                  Bevolxyz(j,i) = Bxyz(j,i)
+               ENDDO
+            ENDDO
+         ELSE
+            STOP 'unknown MHD variable in rdump'
+         ENDIF
+c--real*4
+
+c--real*8
+
+      ELSEIF (imhd.EQ.idim) THEN
+          PRINT *,' no magnetic fields detected in dump file' 
+      ENDIF
 
 c
 c--End reading of dump file
@@ -283,131 +471,21 @@ c                        vxyzu(4,ii) = vxyzu(4,ii) * howmuch
                      ENDIF
                   END DO
                ENDIF
+               
+               IF (iaddmhd.EQ.'y') THEN
+                  CALL setBfield
+               ENDIF
 c
 c--Dump new file
 c
-      int1 = 690706
-      int2 = 780806
-      i1 = int1
-      r1 = real(int2)
-c
-c--Write output file
-c
-      WRITE (7) int1,r1,int2,i1,int1
-      fileident = 'FHydro1'
-      WRITE (7) fileident
-c
-c--Single values
-c
-c--Default int
-      number = 6
-      WRITE (7) number
-      WRITE (7) npart,n1,n2,nreassign,naccrete,nkill
-c--int*1, int*2, int*4, int*8
-      number = 0
-      DO i = 1, 4
-         WRITE (7) number
+      ifulldump = 0
+      DO i = 1, npart
+         isort(i) = i
+         iorig(i) = i
       END DO
-c--Default real
-      number = 14
-      WRITE (7) number
-      WRITE (7) gt, dtmax, gamma, rhozero, RK2,
-     &     escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
-     &     specang, ptmassin
-c--real*4
-      number = 0
-      WRITE (7) number
-c--real*8
-      number = 3
-      WRITE (7) number
-      WRITE (7) udist, umass, utime
-c
-c--Arrays
-c
-c--Number of array lengths
-c
-      number = 2
-      WRITE (7) number
-c
-c--Array length 1 header
-c
-      number8 = npart
-      nums(1) = 1
-      nums(2) = 1
-      nums(3) = 0
-      nums(4) = 0
-      nums(5) = 0
-      nums(6) = 9
-      nums(7) = 2
-      nums(8) = 0
-      WRITE (7) number8, (nums(i), i=1,8)
-c
-c--Array length 2 header
-c
-      number8 = nptmass
-      nums(1) = 1
-      nums(2) = 0
-      nums(3) = 0
-      nums(4) = 0
-      nums(5) = 0
-      nums(6) = 9
-      nums(7) = 0
-      nums(8) = 0
-      WRITE (7) number8, (nums(i), i=1,8)      
-c
-c--Array length 1 arrays
-c      
-c--Default int
-      WRITE (7) (isteps(i), i=1, npart)
-c--int*1
-      WRITE (7) (iphase(i), i=1, npart)
-c--int*2
-
-c--int*4
-
-c--int*8
-
-c--Default real
-      DO j = 1, 5
-         WRITE (7) (xyzmh(j,i), i=1, npart)
-      END DO
-      DO j = 1, 4
-         WRITE (7) (vxyzu(j,i), i=1, npart)
-      END DO      
-c--real*4
-      WRITE (7) (rho(i), i=1, npart)
-      WRITE (7) (dgrav(i), i=1, npart)      
-c     WRITE (7) (alphaMM(i), i=1, npart)
-c--real*8
-
-c
-c--Array length 2 arrays
-c
-
-c--Default int
-      WRITE (7) (listpm(i), i=1,nptmass)
-c--int*1
-
-c--int*2
-
-c--int*4
-
-c--int*8
-
-c--Default real
-      WRITE (7) (spinx(i),i=1,nptmass)
-      WRITE (7) (spiny(i),i=1,nptmass)
-      WRITE (7) (spinz(i),i=1,nptmass)
-      WRITE (7) (angaddx(i),i=1,nptmass)
-      WRITE (7) (angaddy(i),i=1,nptmass)
-      WRITE (7) (angaddz(i),i=1,nptmass)
-      WRITE (7) (spinadx(i),i=1,nptmass)
-      WRITE (7) (spinady(i),i=1,nptmass)
-      WRITE (7) (spinadz(i),i=1,nptmass)
-c--real*4
-
-c--real*8
-
+      nfullstep = 1
+      PRINT *,'writing dump file'
+      CALL wdump(7)
 c
 c--End writing of full dump file
 c-------------------------------

e33a5e07ccdb2e83ce0b03aeca25b2c0223f4983
Matthew Bate <M.R.Bate@exeter.ac.uk>
2006-07-12 16:41:18 +0000

    Added spline softening of gravity for sink particles done using gptall.f
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@223 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/gptall.f
+++ sphNG/gptall.f
@@ -69,7 +69,7 @@ c--Check to see if neighbour - NO POINT MASSES AS NEIGHBOURS
 c
 ccc            hmean2 = ((hipt + xyzmh(5,j))/2.)**2
             hmean2 = hipt**2
-            IF (rr.LT.(4.*hmean2) .AND. iphase(j).EQ.0) THEN
+            IF (rr.LT.hmean2 .AND. iphase(j).EQ.0) THEN
                neigh = neigh + 1
                IF (neigh.GT.iptneigh) CALL error(where,1)
                nearpt(neigh,iptn) = j
@@ -89,18 +89,47 @@ c
 c--Non-pointmass force
 c
 c
-c--Softened potential 1
+c--Softened potential 1 (spline kernel softening)
 c
-               IF(iptsoft.EQ.1) THEN 
+               IF (iptsoft.EQ.1) THEN
+                  hptmass = 0.5*ptsoft
+                  v2 = rr/hptmass**2
+                  IF (v2.LT.4.0) THEN
+                     v1 = SQRT(v2)
+                     rr05 = v1*hptmass
+                     third = 1.0/3.0
+                     IF (v2.LT.1.0) THEN
+                        v4 = v2*v2
+                        fff = pmassj/(rr*rr05)*(4.0*third*v2*v1-
+     &                                           1.2*v4*v1+0.5*v4*v2)
+                        potn = -(pmassj/hptmass*(2.0*third*v2-0.3*v4+
+     &                                                0.1*v4*v1-1.4))
+                     ELSE
+                        v4 = v2*v2
+                        fff = pmassj/(rr*rr05)*(8.0*third*v2*v1-
+     &                    3.0*v4+1.2*v4*v1-0.5*third*v4*v2-0.2*third)
+                        potn = -(pmassj/hptmass*(4.0*third*v2-v2*v1+
+     &                       0.3*v4-0.1*third*v4*v1-1.6) +
+     &                       0.2*third*pmassj/rr05)
+                     ENDIF
+                  ELSE
+                     rr05 = SQRT(rr)
+                     fff = pmassj/(rr*rr05)
+                     potn = pmassj/rr05
+                  ENDIF
+c
+c--Softened potential 2
+c
+               ELSEIF(iptsoft.EQ.2) THEN 
                   rr = rr + ptsoft*ptsoft
                   rrs05 = SQRT(rr)
                   rr32 = rr*rrs05
                   potn = pmassj / rrs05
                   fff = pmassj / rr32
 c
-c--Softened potential 2
+c--Softened potential 3
 c
-               ELSEIF (iptsoft.EQ.2) THEN
+               ELSEIF (iptsoft.EQ.3) THEN
                   rr4 = rr*rr + ptsoft*ptsoft*ptsoft*ptsoft
                   rr4s025 = (rr4)**0.25
                   rr54 = rr4*rr4s025

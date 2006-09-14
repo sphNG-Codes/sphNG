f304a120925ddd0e7d75f8c9ad085ec2de06f5b1
Daniel Price <daniel.price@monash.edu>
2006-09-14 11:43:12 +0000

    options for initial/restart
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@266 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/utils/runjobs_xgrid.sh
+++ sphNG/utils/runjobs_xgrid.sh
@@ -2,6 +2,19 @@
 #
 # script to run a sequence of runs via xgrid
 #
+if [ "$#" -ne 1 ]; then
+  echo Submits multiple runs of sphNG to xgrid server
+  echo by D. Price Sep 2006
+  echo
+  echo Usage: $0 jobtype;
+  echo "where job type is 'initial', 'restart' or 'dummy'";
+else
+jobtype=$1;
+if [ $jobtype == 'initial' ]; then
+   echo initial run;
+elif [ $jobtype == 'dummy' ]; then
+   echo dummy run;
+fi
 rm cleanup
 rm getresults
 for x in `cat fluxtomass_values`; do 
@@ -9,19 +22,24 @@ newdir=mbossbod_f$x
 mkdir $newdir
 cd $newdir
 echo '--------' $newdir '--------'
-#
-# this part writes the input file for each run
-#
 pwd
+#
+# make sure we have the latest code version
+#
+~/sphNG/utils/writemake.tcsh > Makefile
+make gradh
+#
+# set up the run for the first time
+#
+if [ $jobtype == 'initial' ]; then
 cat ../setup_part1.txt > setup$x.txt
 echo 40515 >> setup$x.txt
 cat ../setup_part2.txt >> setup$x.txt
 echo $x >> setup$x.txt
 cat ../setup_part3.txt >> setup$x.txt
 cp ../inspho .
-~/sphNG/utils/writemake.tcsh > Makefile
-make gradh
 ./sph_tree_lf_gradh initial imboss < setup$x.txt
+fi
 #
 # the following part writes a script to run the job under xgrid and submits the job
 #  retaining the job id
@@ -32,6 +50,10 @@ echo cd $PWD >> $newdir.sh;
 echo echo hello '>' hello.out >> $newdir.sh;
 echo ./sph_tree_lf_gradh evolution imboss '>&' $newdir.output >> $newdir.sh
 chmod a+x $newdir.sh
+#
+# for a dummy run do not actually submit the script
+#
+if [ $jobtype != 'dummy' ]; then
 jobid=`xgrid -hostname cytosine.ex.ac.uk -auth Kerberos -job submit ./$newdir.sh`
 echo $jobid
 jobid=${jobid/\{jobIdentifier =/};
@@ -45,5 +67,8 @@ echo xgrid -hostname cytosine.ex.ac.uk -auth Kerberos -out $PWD -se $PWD/$newdir
 echo source $newdir/getresults$newdir >> ../getresults
 echo echo deleting $newdir... >> ../cleanup
 echo xgrid -hostname cytosine.ex.ac.uk -auth Kerberos -job delete -id $jobid >> ../cleanup
+fi
 cd ..
 done;
+
+fi

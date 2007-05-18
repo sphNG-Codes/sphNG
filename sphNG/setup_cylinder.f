00597af14b9c3f6b3129c22fc448e8e1d216ca08
Daniel Price <daniel.price@monash.edu>
2007-05-18 09:20:49 +0000

    current state of tokamak+cylinder setups
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@410 5d52cb8c-a192-40c0-a655-fe23f7e14f03

---
+++ sphNG/setup_cylinder.f
@@ -0,0 +1,197 @@
+      SUBROUTINE setup_cylinder(xyzmh,vxyzu,Bevolxyz,rhozero,
+     &           RK2,npart,ntot)
+c************************************************************
+c                                                           *
+c     Cylindrical setup for Force-Free in cylinder          *     
+c                                                           *
+c************************************************************
+
+
+      IMPLICIT NONE
+      INCLUDE 'idim'
+      INCLUDE 'COMMONS/densi'
+      INCLUDE 'COMMONS/rbnd'
+      INCLUDE 'COMMONS/xforce' 
+      INCLUDE 'COMMONS/cylinder'
+      INCLUDE 'COMMONS/cgas'
+      INCLUDE 'COMMONS/typef'
+      
+      REAL xyzmh(5,idim),vxyzu(4,idim), Bevolxyz(3,imhd)      
+      REAL pi, gama1, rhozero, RK2, Slund, valfven
+      PARAMETER (pi=3.141592653589)
+      
+      INTEGER npart,ntot
+      INTEGER ipart,nr,nz,nc,ir,iz,ic,i, ipfree
+      REAL massp,totvol,totmass
+      REAL deltar,deltaz,rr,angle
+      REAL offset, dbesj0, dbesj1, ran1, zz
+
+c     Set Boundary To be Periodic in z
+      ibound = 12
+233   WRITE (*,*) 'Do you want pressure free cylinder? (0/1)'
+      READ (*,*) ipfree
+      
+      IF (ipfree.EQ.0) THEN
+         encal = 'p'
+         gamma = 5./3.
+         iexf = 10
+         print *, 'encal =', encal, 'gamma =', gamma, 'iexf =',iexf 
+      ELSEIF (ipfree.EQ.1) THEN
+         encal = 'i'
+         gamma = 1.
+         iexf = 0
+         print *, 'encal =', encal, 'gamma =', gamma, 'iexf =',iexf
+      ELSE 
+         GO TO 233
+      ENDIF
+      
+      rcyl = radius     
+      ipart = 0
+      nr = 20
+      nz = int(nr*length/radius)    
+
+      deltar = radius/(nr+0.5)
+      deltaz = length/nz     
+      
+c     Force-free magnetic field configuration, in a cylinder
+c     Muff = 2.5 set in Cylinder common
+      
+      do iz = 1, nz
+         zz = -length/2.+deltaz/2.+deltaz*(iz-1) 
+         print *, 'zz =', zz, iz, deltaz
+         do ir = 1, nr +1
+            rr = deltar*(ir-1) 
+            nc = 2*pi*rr/deltar
+c            write (*,*) 'nc =', nc
+c            if (ir.eq.nr+1) write (*,*) rr, radius-rr, 0.5*deltar
+            if (nc.eq.0) then
+              ipart = ipart +1
+              IF (ipart.GT.idim) STOP 'setup_cyl: dims too small'
+              xyzmh(1,ipart) = 0.d0
+              xyzmh(2,ipart) = 0.d0
+              xyzmh(3,ipart) = zz        
+              if (imhd.eq.idim) then               
+				  Bevolxyz(1,ipart) =  0.d0
+				  Bevolxyz(2,ipart) =  0.d0
+				  Bevolxyz(3,ipart) =  ampl*dbesj0(Muff*rr) 
+              endif
+            else
+              offset = ran1(1)
+              do ic = 1, nc     
+                 angle = 2*pi/nc
+                 ipart = ipart+1
+                 IF (ipart.GT.idim) STOP 'setup_cyl: dims too small'
+                 xyzmh(1,ipart) = rr*COS(angle*(ic-0.5)+offset*angle)
+                 xyzmh(2,ipart) = rr*SIN(angle*(ic-0.5)+offset*angle)
+                 xyzmh(3,ipart) = -length/2.+deltaz/2.+deltaz*(iz-1)    
+                 if (imhd.eq.idim) then 
+                    Bevolxyz(1,ipart) = -ampl*xyzmh(2,ipart)/rr
+     &                                     *dbesj1(Muff*rr)
+                    Bevolxyz(2,ipart) = ampl*xyzmh(1,ipart)/rr
+     &                                     *dbesj1(Muff*rr)                
+                    Bevolxyz(3,ipart) = ampl*dbesj0(Muff*rr) 
+                 endif   
+              enddo
+            endif                
+         enddo
+      enddo              
+      
+      npart = ipart
+      ntot = ipart
+!     choose an initial density
+      rhozero = 1.e-3
+      hzero = 1.2*deltar
+      totvol = (pi*radius**2)*length
+      totmass = rhozero*totvol
+      massp = totmass/npart   
+      RK2 = 1.5
+      gama1 = gamma-1.
+      DO i=1,npart
+         vxyzu(1,i) = 0.
+         vxyzu(2,i) = 0.
+         vxyzu(3,i) = 0.
+         IF (imhd.EQ.idim) THEN
+            vxyzu(1,i) = vxyzu(1,i) + vampl*Bevolxyz(1,i)
+            vxyzu(2,i) = vxyzu(2,i) + vampl*Bevolxyz(2,i)
+            vxyzu(3,i) = vxyzu(3,i) + vampl*Bevolxyz(3,i)   
+         ENDIF   
+         IF (ipfree.EQ.0) THEN
+            vxyzu(4,i) = RK2*(rhozero**gama1)
+         ELSE
+            vxyzu(4,i) = 0.
+         ENDIF
+         xyzmh(4,i) = massp
+         xyzmh(5,i) = hzero
+c-- Setup an initial uniform density         
+         rho(i) = rhozero
+      ENDDO
+
+c-- Estimate Lundquist number
+      valfven = ampl**2/rhozero
+      Slund =radius*valfven/etamhd 
+      
+      WRITE(*,*) 'npart = ',npart,' particle mass = ',massp,
+     &           ' denszero = ',rhozero
+      WRITE(*,*) 'The Lundquist number = t_eta/t_alfven =', Slund
+      
+      RETURN      
+      
+      END
+
+
+
+C      nr = 20
+C      deltar = radius/nr
+C      nz = int(2.0*radius/deltar)    
+C      deltaz = 2.0*radius/nz     
+C      nc1 = 5
+C      steptheta = 0.99999*2.0*pi/nc1
+C      
+C      do ir=1, nr
+C         write (*,*) 'ir=',ir
+C         rr = ir*deltar
+C         nc = int(2.0*pi*rr/(steptheta*deltar))
+C         
+C         write(*,*) 'ntheta',nc
+C         
+C         do iz =1, nz
+C            zi = (iz-0.5)*deltaz - radius
+C            
+C            do ic = 1, nc
+C               ipart = ipart+1
+C               IF (ipart.GT.idim) STOP 'setup_cyl: dims too small'            
+C               angle = (ic-0.5)*steptheta/ir
+C               xyzmh(1,ipart) = rr*COS(angle)
+C               xyzmh(2,ipart) = rr*SIN(angle)
+C               xyzmh(3,ipart) = zi
+C               Bevolxyz(1,ipart) =  -0.001*dbesj1(Muff*rr)
+C     &                                     *xyzmh(2,ipart)/rr
+C               Bevolxyz(2,ipart) =  0.001*dbesj1(Muff*rr)
+C     &                                     *xyzmh(1,ipart)/rr
+C               Bevolxyz(3,ipart) =  0.001*dbesj0(Muff*rr) 
+C            enddo
+C         enddo
+C      enddo
+
+      
+      
+C      do iz = 1,nz
+C         do ix = 0, 2*nr-1
+C            xcyl = - radius + deltar/2.+ix*deltar
+C            do iy = 0,2*nr-1
+C               ycyl = -radius + deltar/2.+iy*deltar
+C               rincyl2 = xcyl*xcyl+ycyl*ycyl
+C               IF (rincyl2.LE.radius**2) THEN
+C                 ipart = ipart +1      
+C                 IF (ipart.GT.idim) STOP 'setup_cyl: dims too small'
+C                 xyzmh(1,ipart) = xcyl
+C                 xyzmh(2,ipart) = ycyl
+C                 xyzmh(3,ipart) = -length/2.+deltaz/2.+deltaz*(iz-1)     
+C                 Bevolxyz(1,ipart) = 0. 
+C                 Bevolxyz(2,ipart) = 0. 
+C                 Bevolxyz(3,ipart) = 0.            
+C               ENDIF
+C            enddo
+C         enddo   
+C      enddo
+            
--- sphNG/setup_torus.f
+++ sphNG/setup_torus.f
@@ -1,4 +1,4 @@
-      SUBROUTINE setup_torus(xyzmh,vxyzu,rhozero,npart,ntot)
+      SUBROUTINE setup_torus(xyzmh,vxyzu,rhozero,RK2, npart,ntot)
 c************************************************************
 c                                                           *
 c     setup for Tokamak torus                               *
@@ -7,25 +7,38 @@ c************************************************************
 
       IMPLICIT NONE
       INCLUDE 'idim'
+      INCLUDE 'COMMONS/xforce'     
+      INCLUDE 'COMMONS/cgas'
       INCLUDE 'COMMONS/tokamak'
 
       REAL xyzmh(5,idim),vxyzu(4,idim)
       REAL pi
       PARAMETER (pi=3.141592653589)
       INTEGER npart,ntot
-      REAL rhozero
+      REAL rhozero, RK2, gama1
       REAL deltaz,deltax,deltaphi,rcyl,rintorus2,phi
       REAL xtorus,ztorus,massp,totmass,totvol
       REAL phitorus, randphi, ran1, thetator, r1, rho_c
-      REAL rintorus, xi, yi, zi, rfactor, kappa
+      REAL rintorus, xi, yi, zi, rfactor, kappa, ra2, pri
       INTEGER ipart,nx,nz,nphi,k,j,i
 
-      nx = 17
-      nz = 17
+      nx = 15
+      nz = 15
       deltaz = 2.*atorus/nz
       deltax = 2.*atorus/nx
       ipart = 0
-
+      encal  = 'i'
+      gamma = 5./3.
+      
+c Particle and eos properties/constants
+      rhozero = 1.
+      hzero = 0.6*deltax
+      ra2 = da2*(atorus-hzero)**2      
+      pri = currj0*currj0*atorus*atorus*(47.-12.*ra2**5+
+     &      75.*ra2**4-200.*ra2**3+270.*ra2**2-180.*ra2)/720.      
+      RK2 = pri/rhozero*gamma
+      gama1 = gamma-1.
+      
       DO k=1,nz
          ztorus = (k-0.5)*deltaz - atorus
          DO j=1,nx        
@@ -48,11 +61,14 @@ c************************************************************
                   xyzmh(1,ipart) = rcyl*COS(phi+randphi)
                   xyzmh(2,ipart) = rcyl*SIN(phi+randphi)
                   xyzmh(3,ipart) = ztorus
+                  ra2 = rintorus2*da2
+                  pri = currj0*currj0*atorus*atorus*(47.-12.*ra2**5+
+     &                 75.*ra2**4-200.*ra2**3+270.*ra2**2-180.*ra2)/720.
 c                 zero velocities
-                  vxyzu(1,ipart) = 0.
-                  vxyzu(2,ipart) = 0.
-                  vxyzu(3,ipart) = 0.
-                  vxyzu(4,ipart) = 0.15 !so that p = rho in the eos routine
+                  vxyzu(1,i) = 0.
+                  vxyzu(2,i) = 0.
+                  vxyzu(3,i) = 0.       
+                  vxyzu(4,ipart) = pri/gama1/rhozero                 
                ENDDO
             ENDIF
          ENDDO
@@ -60,39 +76,44 @@ c                 zero velocities
       
       npart = ipart
       ntot = ipart
-!     choose an initial density
-      totvol = (pi*atorus**2)*2.*pi*Rtorus
-      totmass = (pi*currj0*atorus**2)**2*Rtorus/36.*10.
+c!     choose an initial density
+      totvol = (pi*atorus**2)*2.*pi*Rtorus      
+      totmass = totvol*rhozero !(pi*currj0*atorus**2)**2*Rtorus/36.
       massp = totmass/npart
-      rhozero = totmass/totvol
+c      rhozero = totmass/totvol
       
       DO i=1,npart
          xyzmh(4,i) = massp
-         xyzmh(5,i) = 1.2*deltax
+         xyzmh(5,i) = hzero*2.
+c        zero velocities
+c         vxyzu(1,i) = 0.
+c         vxyzu(2,i) = 0.
+c         vxyzu(3,i) = 0.         
+c         vxyzu(4,i) = RK2*(rhozero**gama1)
       ENDDO
 
 c
 c--Stretching the spatial distribution to have exponential density distribution
-c
-      kappa = 4.
-      rho_c = rhozero*kappa/(1.- EXP(-kappa))
-      DO i = 1, npart
-         xi = xyzmh(1,i)
-         yi = xyzmh(2,i)
-         zi = xyzmh(3,i)
-         rcyl = SQRT(xi*xi+yi*yi)
-         rintorus2 = (rcyl - Rtorus)**2 + zi*zi
-         rintorus = sqrt(rintorus2)
-         IF (rintorus.gt.tiny) THEN
-            phitorus = ATAN2(yi,xi)
-            thetator = ATAN2(zi,rcyl-Rtorus)
-            rfactor = 1.- kappa*rhozero/rho_c*(rintorus/atorus)**2
-            r1 = atorus*SQRT(-LOG(rfactor)/kappa)
-            xyzmh(1,i) = (Rtorus+r1*COS(thetator))*COS(phitorus)
-            xyzmh(2,i) = (Rtorus+r1*COS(thetator))*SIN(phitorus)
-	        xyzmh(3,i) = r1*SIN(thetator)
-	     ENDIF   
-	  END DO        
+c      
+c      kappa = 4.
+c      rho_c = rhozero*kappa/(1.- EXP(-kappa))
+c      DO i = 1, npart
+c         xi = xyzmh(1,i)
+c         yi = xyzmh(2,i)
+c         zi = xyzmh(3,i)
+c         rcyl = SQRT(xi*xi+yi*yi)
+c         rintorus2 = (rcyl - Rtorus)**2 + zi*zi
+c         rintorus = sqrt(rintorus2)
+c         IF (rintorus.gt.tiny) THEN
+c            phitorus = ATAN2(yi,xi)
+c            thetator = ATAN2(zi,rcyl-Rtorus)
+c            rfactor = 1.- kappa*rhozero/rho_c*(rintorus/atorus)**2
+c            r1 = atorus*SQRT(-LOG(rfactor)/kappa)
+c            xyzmh(1,i) = (Rtorus+r1*COS(thetator))*COS(phitorus)
+c            xyzmh(2,i) = (Rtorus+r1*COS(thetator))*SIN(phitorus)
+c	        xyzmh(3,i) = r1*SIN(thetator)
+c	     ENDIF   
+c	  END DO        
 	  
       WRITE(*,*) 'npart = ',npart,' particle mass = ',massp,
      &           ' denszero = ',rhozero

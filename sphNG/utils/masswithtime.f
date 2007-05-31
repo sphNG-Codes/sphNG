a671831d5153a5d4f01c1dee88d9d1f203fba5df
Daniel Price <daniel.price@monash.edu>
2007-05-31 08:40:09 +0000

    also works from dump files
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@424 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/utils/masswithtime.f
+++ sphNG/utils/masswithtime.f
@@ -7,17 +7,37 @@ c                                                           *
 c  DJP May 2007                                             *
 c************************************************************
       IMPLICIT NONE
-      INTEGER, PARAMETER :: idim=100
+      INTEGER, PARAMETER :: idim=36000000
       INTEGER, PARAMETER :: nout=100
+      INTEGER, PARAMETER :: iptmass=100
       INTEGER i,j,nfiles,iargc,ierr,ioutput
-      INTEGER nptmass,ipart,iout
-      REAL pmass(idim),timeff,time,dumi
-      CHARACTER*100 filename,fileout(idim)
+      INTEGER nptmass,ipart,iout,listpm(iptmass)
+      REAL pmasstemp(idim)
+      REAL pmass(iptmass),timeff,time,dumi
+      CHARACTER*100 filename,fileout(iptmass)
+      LOGICAL pfiles
+!--stuff for reading from dump files
+      INTEGER npart,n1,n2,nreassign,naccrete,nkill
+      INTEGER nhydro,idisk1
+      INTEGER nums1(8),nums2(8),nums3(8),nums4(8)
+      LOGICAL smalldump
+      REAL dtmaxdp, gamma, rhozero, RK2
+      REAL escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz
+      REAL specang, ptmassin, pmassinitial
+      REAL*8 umassi, udisti, utimei
+      REAL, PARAMETER :: pi=3.1415926536
 
       nfiles = iargc()
       IF (nfiles.LE.0) THEN
          STOP 'Usage: masswithtime Pfile(s)'
       ENDIF
+
+      pfiles = .false.
+      CALL getarg(1,filename)
+      IF (filename(1:1).EQ.'P') THEN
+         pfiles = .true.
+      ENDIF
+      idisk1 = 15
 c
 c--open file for j vs time plots
 c
@@ -33,42 +53,107 @@ c
       IF (ioutput.EQ.0) THEN
          iout = 55
          DO i=1,nout
-            WRITE(fileout(i),"(a,i3.3,a)") 'sink',i,'.out'
+            IF (pfiles) THEN
+               WRITE(fileout(i),"(a,i3.3,a)") 'sink',i,'.out'
+            ELSE
+               WRITE(fileout(i),"(a,i3.3,a)") 'sinkd',i,'.out'
+            ENDIF
             PRINT*,' opening ',fileout(i)
             OPEN(UNIT=iout+i,FILE=fileout(i),STATUS='replace',
      &           FORM='formatted')
          ENDDO
       ENDIF
       
+      
       DO j=1,nfiles
          CALL getarg(j,filename)
 
          PRINT*,'opening ',filename
-         OPEN (15, FILE=filename, STATUS='unknown', FORM='unformatted')
+         OPEN (idisk1, FILE=filename, STATUS='unknown', 
+     &         FORM='unformatted')
          
          ierr = 0
-         
          DO WHILE (ierr.EQ.0)
-            READ(15,iostat=ierr) timeff,time,nptmass
-            PRINT*,' t_ff = ',timeff,' t = ',time,' nptmass = ',nptmass
-            IF (ierr /= 0) THEN
-                PRINT*,'ERROR READING TIME FROM FILE'
-            ELSE
-               pmass = 0.
-               DO i=1,nptmass
-                  READ(15,iostat=ierr) ipart,dumi,dumi,dumi,
+            IF (pfiles) THEN
+c
+c--get sink properties from sink P file
+c
+               READ(idisk1,iostat=ierr) timeff,time,nptmass
+               PRINT*,' t_ff= ',timeff,' t= ',time,' nptmass= ',nptmass
+               IF (ierr /= 0) THEN
+                  PRINT*,'ERROR READING TIME FROM FILE'
+               ELSE
+                  pmass = 0.
+                  DO i=1,nptmass
+                     READ(idisk1,iostat=ierr) ipart,dumi,dumi,dumi,
      &                                       dumi,dumi,dumi,pmass(i)
-                  !PRINT*,ipart,pmass(i)
-               ENDDO
-               DO i=1,nptmass
-                  WRITE(iout+i,*) timeff,time,pmass(i)
-               ENDDO
-         
-               IF (ierr /= 0) PRINT*,'ERROR READING SINKS FROM FILE'
-            ENDIF
+                     !PRINT*,ipart,pmass(i)
+                  ENDDO
+                  DO i=1,nptmass
+                     WRITE(iout+i,*) timeff,time,pmass(i)
+                  ENDDO
+
+                  IF (ierr /= 0) PRINT*,'ERROR READING SINKS FROM FILE'
+               ENDIF
+            ELSE
+c
+c--get sink properties from dump file
+c
+               CALL read_header(idisk1,ierr,smalldump,idim,
+     &              n1,n2,nreassign,naccrete,nkill,time,dtmaxdp,gamma,
+     &              rhozero,RK2,escap, tkin, tgrav, tterm, anglostx,
+     &              anglosty, anglostz,specang, ptmassin, pmassinitial,
+     &              udisti, umassi, utimei,
+     &              npart,nhydro,nptmass,nums1,nums2,nums3,nums4)
+
+               timeff = time/(SQRT((3. * pi) / (32. * rhozero)))
+               PRINT*,' t_ff= ',timeff,' t= ',time,' nptmass= ',nptmass
+c
+c--skip normal particle block
+c
+               IF (nptmass.GT.0) THEN
+                  IF (smalldump) THEN
+                     DO i=1,SUM(nums1(1:8))
+                        READ(idisk1,iostat=ierr)
+                     ENDDO
+                  ELSE
+                     DO i=1,SUM(nums1(1:5))+3
+                        READ(idisk1,iostat=ierr)
+                     ENDDO
+                     IF (ierr /= 0) PRINT*,'ERROR SKIPPING BLOCKS'
+c
+c--read masses of all particles from full dumps
+c
+                     READ(idisk1,iostat=ierr) (pmasstemp(i),i=1,npart)
+                     DO i=1,nums1(6)-4 + SUM(nums1(7:8))
+                        READ(idisk1,iostat=ierr)
+                     ENDDO
+                  ENDIF
+                  IF (ierr /= 0) PRINT*,'ERROR READING FROM FILE'
+c
+c--read sink masses from block 2 of the dump file
+c
+c
+c--Read array type 2 arrays
+c
+c--Default int
+                  READ (idisk1,iostat=ierr) (listpm(i), i=1,nptmass)
+                  IF (smalldump) THEN
+                     READ (idisk1,iostat=ierr) (pmass(i),i=1,nptmass)
+                  ELSE
+                     DO i = 1,nptmass
+                        pmass(i) = pmasstemp(listpm(i))
+                     ENDDO
+                  ENDIF
+                  DO i=1,nptmass
+                     WRITE(iout+i,*) timeff,time,pmass(i)
+                  ENDDO
+               ENDIF
+               ierr = -1 ! move on to next dump file
+           ENDIF
          ENDDO
          
-         CLOSE(15)
+         CLOSE(idisk1)
 
       ENDDO
       

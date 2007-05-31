d2506d725770be7b049a00af5667dd258afb0ab6
Daniel Price <daniel.price@monash.edu>
2007-05-31 08:41:21 +0000

    read_header now separate routine so can be done separately
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@425 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/utils/readdump_sphNG.f
+++ sphNG/utils/readdump_sphNG.f
@@ -14,17 +14,14 @@ c                                                           *
 c************************************************************
 
       IMPLICIT NONE ! those sweet, sweet words
-      INTEGER i,j,i1i
+      INTEGER i,j
       INTEGER idim,iptdim,idisk1,ierr
       INTEGER npart,n1,n2,nreassign,naccrete,nkill,nptmass
-      INTEGER nhydro,number,jlen
-      INTEGER*4 int1, int2, int1i, int2i, int3i
-      INTEGER*8 number8
+      INTEGER nhydro,jlen
       INTEGER nums1(8),nums2(8),nums3(8),nums4(8)
       INTEGER*1 iphase(idim)
       INTEGER isteps(idim),listpm(iptdim)
-      REAL r1i,hacc
-      REAL gt, dtmaxdp, gamma, rhozero, RK2
+      REAL hacc, gt, dtmaxdp, gamma, rhozero, RK2
       REAL escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz
       REAL specang, ptmassin, tiny, pmassinitial
       REAL xyzmh(5,idim),vxyzu(4,idim)
@@ -32,10 +29,9 @@ c************************************************************
       REAL angaddx(iptdim),angaddy(iptdim),angaddz(iptdim)
       REAL spinadx(iptdim),spinady(iptdim),spinadz(iptdim)
       REAL*4 rho(idim),dummyr4(idim)
-      REAL*8 umassi, udisti, utimei, umagfdi
+      REAL*8 umassi, udisti, utimei
       PARAMETER (tiny=1.e-12)
       LOGICAL smalldump
-      CHARACTER*100 fileident
       CHARACTER(*) filename
 c
 c--unit to open file on
@@ -46,133 +42,11 @@ c
 c
 c--Dump file
 c-------------
-c
-c--Standard numbers
-c
-      int1 = 690706
-      int2 = 780806
-c
-c--Read ouput file
-c
-      READ (idisk1, END=100, iostat=ierr) int1i,r1i,int2i,i1i,int3i
-      IF (int1i.NE.int1 .OR. ierr.NE.0) THEN
-         WRITE (*,*) 'ERROR 1 in rdump: ENDIANNESS wrong?'
-         ierr = 1
-         RETURN
-      ENDIF
-      IF (int2i.NE.int2) THEN
-         WRITE (*,*) 'ERROR 2 in rdump: default real size wrong'
-         PRINT*,int1i,r1i,int2i
-         ierr = 2
-         RETURN
-      ENDIF
-      IF (int3i.NE.int1) THEN
-         WRITE (*,*) 'ERROR 3 in rdump: default integer size wrong'
-         ierr = 3
-         RETURN
-      ENDIF
-      READ (idisk1, END=100) fileident
-      WRITE(*,*) fileident
-c
-c--if file is a small dump, return an error code but still read what
-c  can be read from a small dump
-c
-      IF (fileident(1:1).EQ.'S') THEN
-         smalldump = .true.
-         ierr = -1
-      ELSE
-         smalldump = .false.
-      ENDIF
-c
-c--Single values
-c
-c--Default int
-      READ (idisk1, END=100) number
-      IF (number.LT.6) THEN
-         WRITE (*,*) 'ERROR 4 in rdump: not enough default ints'
-         ierr = 4
-         RETURN
-      ENDIF
-      READ (idisk1, END=100) npart,n1,n2,nreassign,naccrete,nkill
-      IF (npart.GT.idim) THEN
-         WRITE (*,*) 'ERROR in rdump: npart>idim'
-         STOP
-      ELSE
-         WRITE (*,*) 'npart = ',npart
-      ENDIF
-c--int*1, int*2, int*4, int*8
-      DO i = 1, 4
-         READ (idisk1, END=100) number
-      END DO
-c--Default real
-      READ (idisk1, END=100) number
-      IF (number.LT.14) THEN
-         WRITE (*,*) 'ERROR 5 in rdump: not enough default reals'
-         ierr = 5
-         RETURN
-      ENDIF
-      IF (smalldump .and. number.GE.15) THEN
-         READ (idisk1, END=100) gt, dtmaxdp, gamma, rhozero, RK2,
-     &        escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
-     &        specang, ptmassin, pmassinitial
-      ELSE
-         READ (idisk1, END=100) gt, dtmaxdp, gamma, rhozero, RK2,
-     &        escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
-     &        specang, ptmassin
-         pmassinitial = 0.
-      ENDIF
-      WRITE(*,*) 'time = ',gt
-c--real*4
-      READ (idisk1, END=100) number
-c--real*8
-      READ (idisk1, END=100) number
-      IF (number.LT.3) THEN
-         WRITE (*,*) 'ERROR 6 in rdump: nreal8 too small header'
-         ierr = 6
-         RETURN
-      ENDIF
-      READ (idisk1, END=100) udisti, umassi, utimei
-
-c
-c--Arrays
-c
-c--Number of array lengths
-c
-      READ (idisk1, END=100) number
-      IF (number.LT.2 .OR. number.GT.4) THEN
-         WRITE (*,*) 'ERROR 7 in rdump'
-         ierr = 7
-         RETURN
-      ENDIF
-c
-c--Read array type 1 header
-c
-      READ (idisk1, END=100) number8, (nums1(i), i=1,8)
-      IF (number8.LT.npart) THEN
-         ierr = 8
-         WRITE (*,*) 'ERROR 8 in rdump: npart wrong',number8,
-     &           npart
-         RETURN
-      ENDIF
-      nhydro = number8
-c
-c--Read array type 2 header
-c
-      READ (idisk1, END=100) number8, (nums2(i), i=1,8)
-      nptmass = number8
-      WRITE(*,*) 'nptmass = ',nptmass
-c
-c--Read array type 3 header
-c
-      IF (number.GE.3) THEN
-         READ (idisk1, END=100) number8
-      ENDIF
-c
-c--Read array type 4 header
-c
-      IF (number.GE.4) THEN
-         READ (idisk1, END=100)
-      ENDIF
+      CALL read_header(idisk1,ierr,smalldump,idim,
+     &   n1,n2,nreassign,naccrete,nkill,gt,dtmaxdp,gamma,rhozero,RK2,
+     &   escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
+     &   specang, ptmassin, pmassinitial, udisti, umassi, utimei,
+     &   npart,nhydro,nptmass,nums1,nums2,nums3,nums4)
 c
 c--Read array type 1 arrays
 c
@@ -319,3 +193,171 @@ c--real*8
       RETURN
       
       END SUBROUTINE
+
+c************************************************************
+c                                                           *
+c  Separate routine for reading the header only so that     *
+c  this can be done as a separate operation                 *
+c  DJP 30/5/07                                              *
+c                                                           *
+c************************************************************
+
+      SUBROUTINE read_header(idisk1,ierr,smalldump,idim,
+     &   n1,n2,nreassign,naccrete,nkill,gt,dtmaxdp,gamma,rhozero,RK2,
+     &   escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
+     &   specang, ptmassin, pmassinitial, udisti, umassi, utimei,
+     &   npart,nhydro,nptmass,nums1,nums2,nums3,nums4)
+      IMPLICIT NONE
+      INTEGER idisk1,ierr,i1i,idim
+      INTEGER npart,n1,n2,nreassign,naccrete,nkill,nptmass
+      INTEGER nhydro
+      INTEGER nums1(8),nums2(8),nums3(8),nums4(8)
+      LOGICAL smalldump
+      REAL gt, dtmaxdp, gamma, rhozero, RK2
+      REAL escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz
+      REAL specang, ptmassin, tiny, pmassinitial
+      REAL*8 umassi, udisti, utimei
+
+      INTEGER*4 int1, int2, int1i, int2i, int3i
+      INTEGER i,number
+      INTEGER*8 number8
+      REAL r1i
+      CHARACTER*100 fileident
+      PARAMETER (tiny=1.e-12)
+c
+c--Standard numbers
+c
+      int1 = 690706
+      int2 = 780806
+c
+c--Read ouput file
+c
+      READ (idisk1, END=100, iostat=ierr) int1i,r1i,int2i,i1i,int3i
+      IF (int1i.NE.int1 .OR. ierr.NE.0) THEN
+         WRITE (*,*) 'ERROR 1 in rdump: ENDIANNESS wrong?'
+         ierr = 1
+         RETURN
+      ENDIF
+      IF (int2i.NE.int2) THEN
+         WRITE (*,*) 'ERROR 2 in rdump: default real size wrong'
+         PRINT*,int1i,r1i,int2i
+         ierr = 2
+         RETURN
+      ENDIF
+      IF (int3i.NE.int1) THEN
+         WRITE (*,*) 'ERROR 3 in rdump: default integer size wrong'
+         ierr = 3
+         RETURN
+      ENDIF
+      READ (idisk1, END=100) fileident
+      WRITE(*,*) fileident
+c
+c--if file is a small dump, return an error code but still read what
+c  can be read from a small dump
+c
+      IF (fileident(1:1).EQ.'S') THEN
+         smalldump = .true.
+      ELSE
+         smalldump = .false.
+      ENDIF
+c
+c--Single values
+c
+c--Default int
+      READ (idisk1, END=100) number
+      IF (number.LT.6) THEN
+         WRITE (*,*) 'ERROR 4 in rdump: not enough default ints'
+         ierr = 4
+         RETURN
+      ENDIF
+      READ (idisk1, END=100) npart,n1,n2,nreassign,naccrete,nkill
+      IF (npart.GT.idim) THEN
+         WRITE (*,*) 'ERROR in rdump: npart>idim'
+         STOP
+      ELSE
+         WRITE (*,*) 'npart = ',npart
+      ENDIF
+c--int*1, int*2, int*4, int*8
+      DO i = 1, 4
+         READ (idisk1, END=100) number
+      END DO
+c--Default real
+      READ (idisk1, END=100) number
+      IF (number.LT.14) THEN
+         WRITE (*,*) 'ERROR 5 in rdump: not enough default reals'
+         ierr = 5
+         RETURN
+      ENDIF
+      IF (smalldump .and. number.GE.15) THEN
+         READ (idisk1, END=100) gt, dtmaxdp, gamma, rhozero, RK2,
+     &        escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
+     &        specang, ptmassin, pmassinitial
+      ELSE
+         READ (idisk1, END=100) gt, dtmaxdp, gamma, rhozero, RK2,
+     &        escap, tkin, tgrav, tterm, anglostx, anglosty, anglostz,
+     &        specang, ptmassin
+         pmassinitial = 0.
+      ENDIF
+      WRITE(*,*) 'time = ',gt
+c--real*4
+      READ (idisk1, END=100) number
+c--real*8
+      READ (idisk1, END=100) number
+      IF (number.LT.3) THEN
+         WRITE (*,*) 'ERROR 6 in rdump: nreal8 too small header'
+         ierr = 6
+         RETURN
+      ENDIF
+      READ (idisk1, END=100) udisti, umassi, utimei
+
+c
+c--Arrays
+c
+c--Number of array lengths
+c
+      READ (idisk1, END=100) number
+      IF (number.LT.2 .OR. number.GT.4) THEN
+         WRITE (*,*) 'ERROR 7 in rdump'
+         ierr = 7
+         RETURN
+      ENDIF
+c
+c--Read array type 1 header
+c
+      READ (idisk1, END=100) number8, (nums1(i), i=1,8)
+      IF (number8.LT.npart) THEN
+         ierr = 8
+         WRITE (*,*) 'ERROR 8 in rdump: npart wrong',number8,
+     &           npart
+         RETURN
+      ENDIF
+      nhydro = number8
+c
+c--Read array type 2 header
+c
+      READ (idisk1, END=100) number8, (nums2(i), i=1,8)
+      nptmass = number8
+      WRITE(*,*) 'nptmass = ',nptmass
+c
+c--Read array type 3 header
+c
+      IF (number.GE.3) THEN
+         READ (idisk1, END=100) number8
+      ENDIF
+c
+c--Read array type 4 header
+c
+      IF (number.GE.4) THEN
+         READ (idisk1, END=100)
+      ENDIF
+
+      RETURN
+
+100   CONTINUE
+    
+      CLOSE (idisk1)
+      WRITE(*,*) ' end of file reached in header read'
+      ierr = 666
+      RETURN      
+      
+      END SUBROUTINE read_header

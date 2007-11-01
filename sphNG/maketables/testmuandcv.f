8b7f5fb3e0d8f0239ffe4f3386e7c617f1e9867a
Matthew Bate <M.R.Bate@exeter.ac.uk>
2007-11-01 12:33:13 +0000

    Corrected equation of state routines (and hence tables) to match Boley et al. (2007) corrections of Black & Bodenheimer ( 1975 ) . Also, testmuandcv now works in cgs units by default.
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@520 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/maketables/testmuandcv.f
+++ sphNG/maketables/testmuandcv.f
@@ -1,40 +1,49 @@
-		PROGRAM TESTMU
+	PROGRAM TESTMU
 
-		INCLUDE '../COMMONS/tgtbl'
-		INCLUDE '../COMMONS/mutbl'
-		REAL K
+	INCLUDE '../COMMONS/tgtbl'
+	INCLUDE '../COMMONS/mutbl'
+	INCLUDE '../COMMONS/units'
+	REAL K
 
-		PRINT *,"Rho?"
-		READ *,rho
+	REAL*4 rho
 
-      OPEN(UNIT=10,FILE='molmasstbl',FORM='unformatted')
+	umass = 1.0
+	udist = 1.0
+	udens = 1.0
+	utime = 1.0
+	uergg = 1.0
+	uergcc = 1.0
 
-		DO I=1,mumxu
-          READ(10) (mutable(i,j), j=1,mumxrh)
-      END DO
+	PRINT *,"Rho?"
+	READ *,rho
+
+	OPEN(UNIT=10,FILE='molmasstbl',FORM='unformatted')
+
+	DO I=1,mumxu
+	   READ(10) (mutable(i,j), j=1,mumxrh)
+	END DO
 
 
-		OPEN(UNIT=8,FILE='gasttbl',FORM='unformatted')
+	OPEN(UNIT=8,FILE='gasttbl',FORM='unformatted')
       	DO i=1, tgmxu
-      		READ(8) (tgtable(i,j), j=1, tgmxrh)
-c				PRINT *,tgtable(i,2000),i
-      	ENDDO
-		print *,"Done"
+	   READ(8) (tgtable(i,j), j=1, tgmxrh)
+           PRINT *,tgtable(i,2000),i
+      	END DO
+	print *,"Done"
       	CLOSE(8)
 
-	   PRINT *,"Read table"
-      K=7.725
-      DO I=1,mumxu+1000
-         K=K+0.005
-         u=10.0**(REAL(K))
-		   y2=getcv(rho,u) 
-         y1=get1overmu(rho,u)
-!			print *,u,rho,y1
-         WRITE(57,*)  u/y2,1.0/y1,y2
-      ENDDO
+	PRINT *,"Read table"
+	K=7.725
+	DO I=1,mumxu+1000
+	   K=K+0.005
+	   u=10.0**(REAL(K))
+	   y2=getcv(rho,u) 
+	   y1=get1overmu(rho,u)
+!           print *,u,rho,y1
+	   WRITE(57,99001)  u/y2,u,y2,1.0/y1
+99001	   FORMAT (4(1PE12.5,1X))
+	END DO
 
-	
+	CLOSE(10)
 
-      CLOSE(10)
-
- 		END
+	END

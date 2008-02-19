68768412b1aceafca3b5accbe4eaff798abd4c26
Matthew Bate <M.R.Bate@exeter.ac.uk>
2008-02-19 10:57:47 +0000

    ass updated so that radiative transfer works with gradh code.  Number of stored variables (saveuekcdedxyz) also drastically reduced
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@615 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/ass.f
+++ sphNG/ass.f
@@ -1,48 +1,56 @@
-      SUBROUTINE ASS(nlst_in,nlst_end,nlstall,list,dt,itime,npart,
-     $     xyzmh,vxyzu,ekcle,trho,dedxyz)
+      SUBROUTINE ASS(nlst_in,nlst_end,list,dt,itime,
+     $     npart,ntot,xyzmh,vxyzu,ekcle,trho,dedxyz,alphaMM)
 
       INCLUDE 'idim'
       
       DIMENSION xyzmh(5,idim)
       DIMENSION vxyzu(4,idim)
       DIMENSION ekcle(5,iradtrans)
-      REAL*4 trho(idim)
+      REAL*4 trho(idim),alphaMM(idim)
       DIMENSION list(idim)
       DIMENSION dedxyz(3,iradtrans)
 
       INCLUDE 'COMMONS/call'
       INCLUDE 'COMMONS/logun'
+      INCLUDE 'COMMONS/phase'
+      INCLUDE 'COMMONS/radstore'
+      INCLUDE 'COMMONS/curlist'
+      INCLUDE 'COMMONS/timei'
 
-      DIMENSION saveuekcdedxyz(7,idim)
-      
-      CHARACTER*7 where
-      LOGICAL*1 moresweep
+      DIMENSION EUsave(2,iradtrans)
 
-      DATA where/'ass'/
-      WRITE (*,*) 'ASS, icall ',icall,nlstall,nlst_end
+      LOGICAL moresweep
+
+      WRITE (*,*) 'ASS, icall ',icall,nlst_end
 c     &     ,xyzmh(5,1),xyzmh(5,2),xyzmh(5,3),xyzmh(5,4)
 c
 c--Keep original versions of U and E and set cv and kappa
 c
+      istepmindone = imax
 C$OMP PARALLEL DO SCHEDULE (static) default(none)
-C$OMP& shared(list,ekcle,saveuekcdedxyz,vxyzu,trho,nlst_in,nlstall)
-C$OMP& shared(dedxyz)
+C$OMP& shared(list,ekcle,origEU,vxyzu,trho,nlst_in,nlst_end)
+C$OMP& shared(icall,iphase,ifullstep,nlst0,EUsave,isteps)
 C$OMP& private(n,i)
-      DO n = nlst_in, nlstall
+C$OMP& reduction(MIN:istepmindone)
+      DO n = nlst_in, nlst_end
          i = list(n)
+         IF (n.LE.nlst0) THEN
+            istepmindone = MIN(istepmindone,isteps(i))
+            ifullstep(i) = .TRUE.
+         ELSE
+            ifullstep(i) = .FALSE.
+         ENDIF
 
-         ekcle(3,i) = GETCV(trho(i),vxyzu(4,i))
-         ekcle(2,i) = GETKAPPA(vxyzu(4,i),ekcle(3,i),trho(i))
+         IF (iphase(i).EQ.0) THEN
+            origEU(1,i) = ekcle(1,i)
+            origEU(2,i) = vxyzu(4,i)
 
-         saveuekcdedxyz(1,i) = vxyzu(4,i)
-         saveuekcdedxyz(2,i) = ekcle(1,i)
-         saveuekcdedxyz(3,i) = ekcle(2,i)
-         saveuekcdedxyz(4,i) = ekcle(3,i)
-         saveuekcdedxyz(5,i) = dedxyz(1,i)
-         saveuekcdedxyz(6,i) = dedxyz(2,i)
-         saveuekcdedxyz(7,i) = dedxyz(3,i)
+            EUsave(1,i) = ekcle(1,i)
+            EUsave(2,i) = vxyzu(4,i)
+         ENDIF
       END DO
 C$OMP END PARALLEL DO
+      IF (istepmindone.EQ.imax) istepmindone = 0
 c
 c--Loop until has arrived at a solution to the implicit radiative transfer
 c      
@@ -58,19 +66,54 @@ c
             ifrac = ifrac + increment
 
             PRINT *,"DOSEG: ",K,Nstep,ifrac
-c     &           ,increment*dt/Nstep,nlst_in,nlst_end,nlstall,npart,
+c     &           ,increment*dt/Nstep,nlst_in,nlst_end,npart,
 c     &     list(1),ekcle(1,1),ekcle(2,1),xyzmh(5,1),vxyzu(1,1)
-            CALL GSIMPL(increment*dt/Nstep,nlst_in,nlst_end,
-     &           nlstall,npart,list,ekcle,xyzmh,vxyzu,dedxyz,trho,
-     &           moresweep,nit,error)
-            
-            IF (moresweep) GOTO 100
-            IF (ifrac.EQ.Nstep) GOTO 150
-            IF (nit.EQ.1 .AND. MOD(ifrac,2*increment).EQ.0
-     &         .AND.error.LT.0.5*tolerance/increment**2
-     &         .AND.increment.EQ.1) THEN
-               increment = increment * 2
+
+c            IF (icall.EQ.1) THEN
+c            CALL GSIMPL(increment*dt/Nstep,dt,itime,
+c     &           npart,ntot,ekcle,xyzmh,vxyzu,dedxyz,trho,
+c     &           alphaMM,moresweep,nit,error,istepmindone)
+c            ELSE
+            CALL GSIMPLS(increment*dt/Nstep,dt,itime,
+     &           npart,ntot,ekcle,xyzmh,vxyzu,dedxyz,trho,
+     &           alphaMM,moresweep,nit,error,istepmindone)
+c            ENDIF
+
+c            IF (moresweep) GOTO 100
+
+            IF (moresweep) THEN
+         print *,'Integration failed - using U & E values anyway ',
+     &              nlst_end
+       WRITE(iprint,*) 'Integration failed - using U & E values anyway',
+     &        nlst_end
+               GOTO 150
             ENDIF
+
+
+            IF (ifrac.EQ.Nstep) GOTO 150
+c            IF (nit.EQ.1 .AND. MOD(ifrac,2*increment).EQ.0
+c     &         .AND.error.LT.0.5*tolerance/increment**2
+c     &         .AND.increment.EQ.1) THEN
+c               increment = increment * 2
+c            ENDIF
+
+C$OMP PARALLEL DO SCHEDULE (static) default(none)
+C$OMP& shared(list,ekcle,origEU,vxyzu,trho,nlst_in,nlst_end)
+C$OMP& shared(iphase)
+C$OMP& private(n,i)
+            DO n = nlst_in, nlst_end
+               i = list(n)
+               IF (iphase(i).EQ.0) THEN
+                  origEU(1,i) = ekcle(1,i)
+                  origEU(2,i) = vxyzu(4,i)
+
+                  ekcle(3,i) = GETCV(trho(i),vxyzu(4,i))
+                  ekcle(2,i) = GETKAPPA(vxyzu(4,i),ekcle(3,i),trho(i))
+               ENDIF
+            END DO
+C$OMP END PARALLEL DO
+            
+
          END DO
 c
 c--Integration failed, increase number of sub-steps and restore initial state
@@ -81,20 +124,25 @@ c
             CALL quit
          ENDIF
 C$OMP PARALLEL DO SCHEDULE (static) default(none)
-C$OMP& shared(list,ekcle,saveuekcdedxyz,vxyzu,trho,nlst_in,nlstall)
-C$OMP& shared(dedxyz)
+C$OMP& shared(list,ekcle,origEU,vxyzu,trho,nlst_in,nlst_end)
+C$OMP& shared(iphase,EUsave)
 C$OMP& private(n,i)
-         DO n = nlst_in, nlstall
+         DO n = nlst_in, nlst_end
             i = list(n)
-            vxyzu(4,i) = saveuekcdedxyz(1,i)
-            ekcle(1,i) = saveuekcdedxyz(2,i)
-            ekcle(2,i) = saveuekcdedxyz(3,i)
-            ekcle(3,i) = saveuekcdedxyz(4,i)
-            dedxyz(1,i) = saveuekcdedxyz(5,i)
-            dedxyz(2,i) = saveuekcdedxyz(6,i)
-            dedxyz(3,i) = saveuekcdedxyz(7,i)
+            IF (iphase(i).EQ.0) THEN
+               ekcle(1,i) = EUsave(1,i)
+               vxyzu(4,i) = EUsave(2,i)
+
+               ekcle(3,i) = GETCV(trho(i),vxyzu(4,i))
+               ekcle(2,i) = GETKAPPA(vxyzu(4,i),ekcle(3,i),trho(i))
+            ENDIF
          END DO
 C$OMP END PARALLEL DO
+
+c         print *,'Integration failed - kept old U & E values',nlst_end
+c         WRITE (iprint,*) 'Integration failed - kept old U & E values',
+c     &        nlst_end
+c         GOTO 150
       END DO
       
  150  CONTINUE

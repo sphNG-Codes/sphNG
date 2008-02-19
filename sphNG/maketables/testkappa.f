a4a992ace9db2a8a39bad424ea56c2c08e540407
Matthew Bate <M.R.Bate@exeter.ac.uk>
2008-02-19 10:55:03 +0000

    Tidied up testkappa program
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@614 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/maketables/testkappa.f
+++ sphNG/maketables/testkappa.f
@@ -1,29 +1,40 @@
-		PROGRAM TESTKAPPA
+	PROGRAM TESTKAPPA
 
-		INCLUDE '../COMMONS/tgtbl'
-		INCLUDE '../COMMONS/optbl'
+	INCLUDE '../COMMONS/tgtbl'
+	INCLUDE '../COMMONS/optbl'
+	INCLUDE '../COMMONS/units'
 
-      OPEN(UNIT=8,FILE='/home/scw/tables/opacitytbl')
+	REAL*4 rho
+
+	umass = 1.0
+        udist = 1.0
+        udens = 1.0
+        utime = 1.0
+        uergg = 1.0
+        uergcc = 1.0
+
+	OPEN(UNIT=8,FILE='/home/mrbate/tables/opacitytbl')
       	DO i=1, opmxtg
-      		READ(8,*) (optable(i,j), j=1, opmxrh)
+	   READ(8,*) (optable(i,j), j=1, opmxrh)
       	ENDDO
-      CLOSE(8)
+	CLOSE(8)
 
-      OPEN(UNIT=8,FILE='/home/scw/tables/gasttbl',FORM='unformatted')
+	OPEN(UNIT=8,FILE='/home/mrbate/tables/gasttbl',
+     &   FORM='unformatted')
       	DO i=1, tgmxu
-      		READ(8) (tgtable(i,j), j=1, tgmxrh)
+	   READ(8) (tgtable(i,j), j=1, tgmxrh)
       	ENDDO
-      CLOSE(8)
+	CLOSE(8)
  
-		PRINT *,"Enter rho:"
-		READ *,rho
-		startu = 7.725
-		DO K=1,1300
-			startu = startu + 0.005
-			u = 10.0**DBLE(startu)
-			cv = getcv(rho,u)
-			rk = getkappa(u,cv,rho)
-			WRITE(30,*) u/cv,rk
-		ENDDO
+	PRINT *,"Enter rho:"
+	READ *,rho
+	startu = 7.725
+	DO K=1,1300
+	   startu = startu + 0.005
+	   u = 10.0**DBLE(startu)
+	   cv = getcv(rho,u)
+	   rk = getkappa(u,cv,rho)
+	   WRITE(30,*) u/cv,rk
+	ENDDO
 
-		END
+	END

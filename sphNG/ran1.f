615bae586309e06003b3e79ff8d76b301a27a8d0
Ben Ayliffe <ayliffe@astro.ex.ac.uk>
2008-04-22 17:42:16 +0000

    Make random number generator resume on restart
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@685 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/ran1.f
+++ sphNG/ran1.f
@@ -12,21 +12,19 @@ c************************************************************
 
       INCLUDE 'COMMONS/savernd'
 
-      INTEGER iseed, IA, IM, IQ, IR, NTAB, NDIV
+      INTEGER iseed, IA, IM, IQ, IR, NDIV
       REAL ran1, AM, EPS, RNMX
 
       PARAMETER (IA=16807, IM=2147483647, AM=1./IM, IQ=127773, IR=2836,
-     &     NTAB=32, NDIV=1+(IM-1)/NTAB, EPS=1.2e-7, RNMX=1.-EPS)
+     &     NDIV=1+(IM-1)/NTAB, EPS=1.2e-7, RNMX=1.-EPS)
 
-      INTEGER j,k,iv(NTAB),iy
-      SAVE iv, iy
-      DATA iv /NTAB*0/, iy /0/
+      INTEGER j,k
 
       IF (iseed.LE.0) THEN
          idum = iseed
       ENDIF
 
-      IF (idum.LE.0 .OR. iy.EQ.0) THEN
+      IF (idum.LE.0 .OR. iyr.EQ.0) THEN
          idum=MAX(-idum, 1)
          DO j = NTAB + 8, 1, -1
             k = idum/IQ
@@ -34,15 +32,15 @@ c************************************************************
             IF (idum.LT.0) idum = idum + IM
             IF (j.LE.NTAB) iv(j) = idum
          END DO
-         iy = iv(1)
+         iyr = iv(1)
       ENDIF
       k = idum/IQ
       idum = IA*(idum - k*IQ) - IR*k
       IF (idum.LT.0) idum = idum + IM
-      j = 1 + iy/NDIV
-      iy = iv(j)
+      j = 1 + iyr/NDIV
+      iyr = iv(j)
       iv(j) = idum
-      ran1 = MIN(AM*iy, RNMX)
+      ran1 = MIN(AM*iyr, RNMX)
  
       RETURN
       END

cf5a140fadd04adf64b0d6fe1ac7d4b3dd8e3435
Matthew Bate <M.R.Bate@exeter.ac.uk>
2008-07-09 22:00:49 +0000

    mtree was not building particles with iphase>=10.  This has been fixed.  Also treef was not finding neighbours of sink particles with iphase>=10.  This has also been fixed.
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@752 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/mtree_P.f
+++ sphNG/mtree_P.f
@@ -40,8 +40,9 @@ c99001 FORMAT (' entry subroutine mtree')
       natom = nnatom
       nactatom = 0
       DO j = 1, natom
-         IF (iphase(j).EQ.0 .OR. (iphase(j).GE.1 .AND. 
-     &                    iphase(j).LT.10 .AND. iptintree.GT.0)) THEN
+         IF (iphase(j).EQ.0 .OR. iphase(j).EQ.10 .OR. 
+     &        (iphase(j).GE.1 .AND. iphase(j).LT.10 .AND. 
+     &        iptintree.GT.0)) THEN
             nactatom = nactatom + 1
             listmap(nactatom) = j
          ENDIF

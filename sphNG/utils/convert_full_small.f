5e2615e265fc18794cd6a1b804f62f2593a0d792
Matthew Bate <M.R.Bate@exeter.ac.uk>
2009-01-30 13:41:35 +0000

    Modification to util/convert_full_small.f that means that in making a full dump into a small dump you can also delete all inactive particles (i.e. those with iphase=-1)
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@856 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/utils/convert_full_small.f
+++ sphNG/utils/convert_full_small.f
@@ -10,12 +10,15 @@ c
       INCLUDE '../COMMONS/units'
       INCLUDE '../COMMONS/sort'
       INCLUDE '../COMMONS/stepopt'
+      INCLUDE '../COMMONS/phase'
+      INCLUDE '../COMMONS/part'
+      INCLUDE '../COMMONS/ptmass'
 
       REAL*8 umassi, udisti, utimei, umagfdi
       COMMON /unitsin/ umassi, udisti, utimei, umagfdi
       COMMON /dtmaxin/ dtmaxdp
 
-      CHARACTER*1 ianswer
+      CHARACTER*1 ianswer,ianswer2
       CHARACTER*7 fullname(maxnumber)
       CHARACTER*9 smallname
 
@@ -24,6 +27,10 @@ c
       WRITE (*,*) '    will have the original name appended with .s)'
       READ (*,88001) ianswer
 88001 FORMAT(A1)
+
+      WRITE (*,*) 'Do you wish to remove inactive particles ',
+     &     '(iphase=-1)?'
+      READ (*,88001) ianswer2
 
       WRITE (*,*) 'How many full dumps?'
       READ (*,*) numfulldumps
@@ -62,6 +69,26 @@ c      READ (*,*) fullname
             STOP
          ENDIF
 
+         IF (ianswer2.EQ.'y') THEN
+            DO i = 1, nptmass
+               listrealpm(listpm(i)) = i
+            END DO
+
+            icountparticles = 0
+            DO i = 1, npart
+               IF (iphase(i).NE.-1) THEN
+                  icountparticles = icountparticles+1
+                  IF (i.NE.icountparticles) THEN
+                     CALL move_particle(i,icountparticles)
+                  ENDIF
+               ENDIF
+            END DO
+            WRITE (*,*) 'New number of particles: ',icountparticles,
+     &           npart
+            npart = icountparticles
+            n1 = npart
+         ENDIF
+
          umass = umassi
          udist = udisti
          utime = utimei
@@ -88,3 +115,15 @@ c      READ (*,*) fullname
       SUBROUTINE endrun
       CALL quit
       END
+
+      SUBROUTINE ghostp(ntot,npart,xyzmh,vxyzu,ekcle,Bevolxyz)
+
+      STOP
+      END
+
+      SUBROUTINE insulate(ival, ntot, npart, dumxyzmh, f1vxyzu)
+
+      STOP
+      END
+
+

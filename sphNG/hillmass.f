9cd4527f03882544acb7f3cb4234babcce8bcf1e
Ben Ayliffe <ayliffe@astro.ex.ac.uk>
2011-04-07 08:25:52 +0000

    Modified hillmas to accept new arguments and calculate mass for rotating potential
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1178 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/hillmass.f
+++ sphNG/hillmass.f
@@ -1,4 +1,4 @@
-      FUNCTION hillmass(planetmass, sswitch)
+      FUNCTION hillmass(planetmass, sswitch, xmass)
 
 c--Added sswitch such that this calculation now works for sink
 c  particles as well as fixed potentials.
@@ -10,20 +10,32 @@ c  particles as well as fixed potentials.
       INCLUDE 'COMMONS/treecom_P'
       INCLUDE 'COMMONS/sort'
       INCLUDE 'COMMONS/ptmass'
+      INCLUDE 'COMMONS/typef'
+      INCLUDE 'COMMONS/pxpy'
 
       EQUIVALENCE (ilist, next1)
       EQUIVALENCE (rr, tempsort)
 
-      REAL rr(idim), hillr, hillm, hillmass, planetmass
+      REAL rr(idim), hillr, hillm, hillmass, planetmass, xmass
       INTEGER ilist(idim), sswitch
 
       hillm = planetmass
-      hillr = (planetmass/3.0)**(1.0/3.0)
+      hillr = (hillm/(3.*xmass))**(1.0/3.0)
+
+      IF (irotpot.EQ.1 .AND. (px.EQ.0.0 .AND. py.EQ.0.0)) THEN
+         print *, 'px and py = 0 in externf.'
+         STOP
+      ENDIF
 
       DO i = 1, npart
          IF (sswitch.EQ.0) THEN
-            rr(i) = SQRT((xyzmh(1,i)-1.0)**2 + xyzmh(2,i)**2 +
-     &           xyzmh(3,i)**2)
+            IF (irotpot.EQ.0) THEN
+               rr(i) = SQRT((xyzmh(1,i)-1.0)**2 + xyzmh(2,i)**2 +
+     &              xyzmh(3,i)**2)
+            ELSE
+               rr(i) = SQRT((xyzmh(1,i)-px)**2 + (xyzmh(2,i)-py)**2 +
+     &              xyzmh(3,i)**2)
+            ENDIF
          ELSEIF (sswitch.EQ.1) THEN
             rr(i) = SQRT((xyzmh(1,i)-xyzmh(1,listpm(1)))**2 +
      &           (xyzmh(2,i) - xyzmh(2,listpm(1)))**2 +
@@ -37,11 +49,10 @@ c  particles as well as fixed potentials.
          i = ilist(j)
          IF (iphase(i).EQ.0) THEN
             IF(rr(i).LE.hillr) hillm = hillm + xyzmh(4,i)
-            hillr = (hillm/3.0)**(1.0/3.0)
+            hillr = (hillm/(3.*xmass))**(1.0/3.0)
          ENDIF
       END DO
 
       hillmass = hillm
-c      hillmass = 0.0001
 
       END FUNCTION hillmass

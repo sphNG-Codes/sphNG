9b7b70cb5b48c98c90bf013816a2eb69fbf4251b
Ben Ayliffe <ayliffe@astro.ex.ac.uk>
2011-04-07 07:47:13 +0000

    Orbits now set using xmass
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1168 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/ghostp100.f
+++ sphNG/ghostp100.f
@@ -25,6 +25,7 @@ c************************************************************
       INCLUDE 'COMMONS/physcon'
       INCLUDE 'COMMONS/astrcon'
       INCLUDE 'COMMONS/cgas'
+      INCLUDE 'COMMONS/xforce'
 
       REAL*4 rhoreal4
 
@@ -91,7 +92,7 @@ c
             xyzmh(3,nptot) = zi
             xyzmh(4,nptot) = pmassi
             xyzmh(5,nptot) = hi
-            vg = 1.0/SQRT(rgr)
+            vg = xmass/SQRT(rgr)
             ang1 = ATAN2(yi,xi)
             vxyzu(1,nptot) = - vg*SIN(ang1) + xyzmh(2,nptot)
             vxyzu(2,nptot) = vg*COS(ang1) - xyzmh(1,nptot)
@@ -116,7 +117,7 @@ c
                   xyzmh(3,nptot) = zi
                   xyzmh(4,nptot) = pmassi
                   xyzmh(5,nptot) = hi
-                  vg = 1.0/SQRT(rgr)
+                  vg = xmass/SQRT(rgr)
                   ang1 = ATAN2(yi,xi)
                   vxyzu(1,nptot) = - vg*SIN(ang1) + xyzmh(2,nptot)
                   vxyzu(2,nptot) = vg*COS(ang1) - xyzmh(1,nptot)
@@ -146,7 +147,7 @@ c
             xyzmh(3,nptot) = zi
             xyzmh(4,nptot) = pmassi
             xyzmh(5,nptot) = hi
-            vg = 1.0/SQRT(r)
+            vg = xmass/SQRT(r)
             vxyzu(1,nptot) = - vg*SIN(ang2) + xyzmh(2,nptot)
             vxyzu(2,nptot) = vg*COS(ang2) - xyzmh(1,nptot)
             vxyzu(3,nptot) = 0.0
@@ -166,7 +167,7 @@ c
             xyzmh(3,nptot) = zi
             xyzmh(4,nptot) = pmassi
             xyzmh(5,nptot) = hi
-            vg = 1.0/SQRT(r)
+            vg = xmass/SQRT(r)
             vxyzu(1,nptot) = - vg*SIN(ang2) + xyzmh(2,nptot)
             vxyzu(2,nptot) = vg*COS(ang2) - xyzmh(1,nptot)
             vxyzu(3,nptot) = 0.0

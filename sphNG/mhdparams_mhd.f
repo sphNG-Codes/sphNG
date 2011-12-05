d50dc5930be6bdb0dc241c1a057145812e49c854
Daniel Price <daniel.price@monash.edu>
2011-12-05 22:30:33 +0000

    mhd params not computed using dead particles or sinks
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1269 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/mhdparams_mhd.f
+++ sphNG/mhdparams_mhd.f
@@ -20,6 +20,7 @@ c************************************************************
       INCLUDE 'COMMONS/varmhd'
       INCLUDE 'COMMONS/numpa'
       INCLUDE 'COMMONS/dissi'
+      INCLUDE 'COMMONS/phase'
       REAL*8 etai
       REAL rhoi
 c
@@ -56,13 +57,16 @@ c
       etarealoverartmax = 0.
       ietacount = 0
       
+      np = 0
 c
 c--Calculate quantities
 c
       DO i=1,npart
-         rhoi = rho(i)
-         rho1i = 0.
-         IF (rhoi.NE.0) rho1i = 1./rhoi
+         IF (iphase(i).GE.0) THEN
+            np = np + 1
+            rhoi = rho(i)
+            rho1i = 0.
+            IF (rhoi.NE.0) rho1i = 1./rhoi
 c         IF (varmhd.EQ.'Bvol') THEN
 c            Bxi = Bevolxyz(1,i)
 c            Byi = Bevolxyz(2,i)
@@ -72,105 +76,107 @@ c            Bxi = Bevolxyz(1,i)*rho(i)
 c            Byi = Bevolxyz(2,i)*rho(i)
 c            Bzi = Bevolxyz(3,i)*rho(i)
 c         ELSE
-            Bxi = Bxyz(1,i)
-            Byi = Bxyz(2,i)
-            Bzi = Bxyz(3,i)
+               Bxi = Bxyz(1,i)
+               Byi = Bxyz(2,i)
+               Bzi = Bxyz(3,i)
 c         ENDIF
 
-         B2i = Bxi**2 + Byi**2 + Bzi**2
-         Bi = SQRT(B2i)
-         IF (Bi.GT.0.) THEN
-            Bi1 = 1./Bi
-            betamhdi = pr(i)/(0.5*B2i)
-         ELSE
-            Bi1 = 0.
-            betamhdi = 0.
-         ENDIF
+            B2i = Bxi**2 + Byi**2 + Bzi**2
+            Bi = SQRT(B2i)
+            IF (Bi.GT.0.) THEN
+               Bi1 = 1./Bi
+               betamhdi = pr(i)/(0.5*B2i)
+            ELSE
+               Bi1 = 0.
+               betamhdi = 0.
+            ENDIF
 c
 c--Max/min/ave B
 c         
-         IF (Bi.LT.Bmin) Bmin = Bi
-         IF (Bi.GT.Bmax) Bmax = Bi
-         Bmean = Bmean + Bi
+            IF (Bi.LT.Bmin) Bmin = Bi
+            IF (Bi.GT.Bmax) Bmax = Bi
+            Bmean = Bmean + Bi
 c
 c--Plasma beta minimum/maximum/average
 c         
-         betamhdav = betamhdav + betamhdi
-         IF (betamhdi.GT.betamhdmax) betamhdmax = betamhdi
-         IF (betamhdi.LT.betamhdmin) betamhdmin = betamhdi
+            betamhdav = betamhdav + betamhdi
+            IF (betamhdi.GT.betamhdmax) betamhdmax = betamhdi
+            IF (betamhdi.LT.betamhdmin) betamhdmin = betamhdi
 c
 c--Maximum/average divergence of B
 c         
-         divBi = abs(divcurlB(1,i))
-         IF (divBi.GT.divBmax) divBmax = divBi
-         divBav = divBav + divBi
+            divBi = abs(divcurlB(1,i))
+            IF (divBi.GT.divBmax) divBmax = divBi
+            divBav = divBav + divBi
 c
 c--max/av magnitude of current
 c
-         curlBxi= divcurlB(2,i)
-         curlByi= divcurlB(3,i)
-         curlBzi= divcurlB(4,i)
-         curlBmag2i= curlBxi*curlBxi + curlByi*curlByi + curlBzi*curlBzi
-         curlBmagi= SQRT(curlBmag2i)
-         curlBav= curlBav + curlBmagi
-         IF (curlBmagi.GT.curlBmax) curlBmax= curlBmagi
+            curlBxi= divcurlB(2,i)
+            curlByi= divcurlB(3,i)
+            curlBzi= divcurlB(4,i)
+            curlBmag2i= curlBxi*curlBxi + curlByi*curlByi 
+     &                                  + curlBzi*curlBzi
+            curlBmagi= SQRT(curlBmag2i)
+            curlBav= curlBav + curlBmagi
+            IF (curlBmagi.GT.curlBmax) curlBmax= curlBmagi
 c
 c--ratio of divB to curlB
 c
-         div2curlBi= divBi/(curlBmagi + tiny)
-         div2curlBav= div2curlBav + div2curlBi
-         IF (div2curlBi.GT.div2curlBmax) div2curlBmax= div2curlBi
+            div2curlBi= divBi/(curlBmagi + tiny)
+            div2curlBav= div2curlBav + div2curlBi
+            IF (div2curlBi.GT.div2curlBmax) div2curlBmax= div2curlBi
 c
 c--|div B| x smoothing length / |B| (see e.g. Cerqueira and Gouveia del Pino 1999) 
 c  this quantity should be less than ~0.01.
 c
-         omegamhdi = divBi*xyzmh(5,i)*Bi1
-         IF (omegamhdi.LT.omegtol) fracdivBok = fracdivBok + 1.
-         IF (omegamhdi.GT.omegamhdmax) omegamhdmax = omegamhdi
-         omegamhdav = omegamhdav + omegamhdi
+            omegamhdi = divBi*xyzmh(5,i)*Bi1
+            IF (omegamhdi.LT.omegtol) fracdivBok = fracdivBok + 1.
+            IF (omegamhdi.GT.omegamhdmax) omegamhdmax = omegamhdi
+            omegamhdav = omegamhdav + omegamhdi
 c
 c--Conserved magnetic flux (int B dV)
 c
-         pmassi = xyzmh(4,i)
-         fluxtot = fluxtot + pmassi*divcurlB(1,i)*rho1i
+            pmassi = xyzmh(4,i)
+            fluxtot = fluxtot + pmassi*divcurlB(1,i)*rho1i
 c
 c--Conserved Cross Helicity (int v.B dV)
 c
-         crosshel = crosshel + pmassi*(vxyzu(1,i)*Bxi 
-     &                         + vxyzu(2,i)*Byi + vxyzu(3,i)*Bzi)*rho1i
+            crosshel = crosshel + pmassi*(vxyzu(1,i)*Bxi 
+     &                          + vxyzu(2,i)*Byi + vxyzu(3,i)*Bzi)*rho1i
 c
 c--magnetic resistivity parameter
 c
-         valphaBmax = MAX(valphaBmax, alphaMM(2,i))
-         valphaBmin = MIN(valphaBmin, alphaMM(2,i))
-         
-c         IF (iresist.GE.2) THEN
-            etai = etafunc(rhoi,vxyzu(4,i),etamhd,iresist)
-            etamax = MAX(etamax, etai)
-            etamin = MIN(etamin, etai)
-            etaav = etaav + etai
-            
-            vsigi = sqrt(vsound(i)**2 + B2i*rho1i)
-            IF (ifsvi.EQ.6) THEN
-               etaart = alphaMM(2,i)*vsigi*xyzmh(5,i)
-            ELSE
-               etaart = alphamin(2)*vsigi*xyzmh(5,i)
-            ENDIF
+            valphaBmax = MAX(valphaBmax, alphaMM(2,i))
+            valphaBmin = MIN(valphaBmin, alphaMM(2,i))
 
-            etaartificial(i) = etaart
-            etareal(i) = etai
+c            IF (iresist.GE.2) THEN
+               etai = etafunc(rhoi,vxyzu(4,i),etamhd,iresist)
+               etamax = MAX(etamax, etai)
+               etamin = MIN(etamin, etai)
+               etaav = etaav + etai
 
-            IF (etaart.GT.0.) THEN
-               ratio = etai/etaart
-               etarealoverartmin = min(etarealoverartmin,ratio)
-               etarealoverartmax = max(etarealoverartmax,ratio)
-               etarealoverartav = etarealoverartav + ratio
-               ietacount = ietacount + 1
-            ENDIF
-c         ENDIF
+               vsigi = sqrt(vsound(i)**2 + B2i*rho1i)
+               IF (ifsvi.EQ.6) THEN
+                  etaart = alphaMM(2,i)*vsigi*xyzmh(5,i)
+               ELSE
+                  etaart = alphamin(2)*vsigi*xyzmh(5,i)
+               ENDIF
+
+               etaartificial(i) = etaart
+               etareal(i) = etai
+
+               IF (etaart.GT.0.) THEN
+                  ratio = etai/etaart
+                  etarealoverartmin = min(etarealoverartmin,ratio)
+                  etarealoverartmax = max(etarealoverartmax,ratio)
+                  etarealoverartav = etarealoverartav + ratio
+                  ietacount = ietacount + 1
+               ENDIF
+c           ENDIF
+         ENDIF
       ENDDO
       
-      denom = 1./FLOAT(npart)
+      denom = 1./FLOAT(np)
       betamhdav = betamhdav*denom
       fracdivBok = 100.*fracdivBok*denom
       omegamhdav = omegamhdav*denom

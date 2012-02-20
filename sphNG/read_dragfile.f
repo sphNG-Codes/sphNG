ee8b721e94b4f3a766fb997b99c3c605c11bf02a
Ben Ayliffe <ayliffe@astro.ex.ac.uk>
2012-02-20 17:44:58 +0000

    Reading and writing of the drag energy file.
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1394 5d52cb8c-a192-40c0-a655-fe23f7e14f03

---
+++ sphNG/read_dragfile.f
@@ -0,0 +1,88 @@
+      subroutine read_dragtable
+
+      IMPLICIT NONE
+
+      INCLUDE 'idim'
+
+      INCLUDE 'COMMONS/radtran3'
+      INCLUDE 'COMMONS/xforce'
+      INCLUDE 'COMMONS/logun'
+      
+      INTEGER i, j, k
+      INTEGER ires_r, ires_p, ires_t
+      REAL trmin, trmax, tdr
+      REAL tpmin, tpmax, tdp
+      REAL ttmin, ttmax, tdt
+      REAL dragpmass, draghmass
+      REAL tol
+
+      tol = 1.0E-2
+c
+c--Read in the resolution and check it matches current code
+c
+      READ (idragfile, ERR=100) ires_r, ires_p, ires_t
+
+      IF (ires_r.NE.idragresr .OR. ires_p.NE.idragresp .OR.
+     &     ires_t.NE.idragrest) THEN
+ 111     FORMAT('Drag table resolution being read in does not ', $,
+     &        'match resolution set in radtran3',/)
+ 112     FORMAT('(code, table) ', A2, ' = ', I6, 1X, I6)
+         WRITE (iprint,111)
+         WRITE (iprint,112) 'ir', idragresr, ires_r
+         WRITE (iprint,112) 'ip', idragresp, ires_p
+         WRITE (iprint,112) 'it', idragrest, ires_t
+         STOP
+      ENDIF
+         
+c
+c--Read in spatial scales and check they match the current code
+c
+      READ (idragfile, ERR=100) trmin, trmax, tdr, 
+     &     tpmin, tpmax, tdp, ttmin, ttmax, tdt
+
+      IF (abs(trmin-dragtrmin).GT.tol .OR.
+     &     abs(trmax-dragtrmax).GT.tol .OR.
+     &     abs(tdr-dragtdr).GT.tol) THEN
+ 113     FORMAT ('Drag table: Radial scale changed')
+ 114     FORMAT ('(read in) min, max, delta ', 3(1PE12.5,1X))
+ 115     FORMAT ('(new    ) min, max, delta ', 3(1PE12.5,1X))
+         WRITE (iprint,113)
+         WRITE (iprint,114) trmin, trmax, tdr
+         WRITE (iprint,115) dragtrmin, dragtrmax, dragtdr
+         STOP
+      ENDIF
+c
+c--Read in planet mass, and check it matches current state
+c
+      READ (idragfile, ERR=100) dragpmass, draghmass
+      
+      IF (dragpmass.NE.planetmass) THEN
+ 116     FORMAT ('Planet mass does not match in dragfile')
+ 117     FORMAT ('(read in here   ) mass ', 1PE15.8)
+ 118     FORMAT ('(read in options) mass ', 1PE15.8)
+         WRITE (iprint,117) dragpmass
+         WRITE (iprint,118) planetmass
+         WRITE (iprint, *) 'Drag file mass overwrites ifile'
+         planetmass = dragpmass
+      ENDIF
+
+c
+c--If all checks successful, repopulate the table from file
+c
+      do i = 1, idragresr
+         do j = 1, idragresp
+            do k = 1, idragrest
+               READ (idragfile, ERR=100) dragenergy(i,j,k)
+            enddo
+         enddo
+      enddo
+
+      CLOSE (idragfile)
+
+      RETURN
+      
+ 100  print *, 'An error has occurred reading drag file'
+      STOP
+
+      end subroutine read_dragtable
+
---
+++ sphNG/write_dragtable.f
@@ -0,0 +1,37 @@
+      subroutine write_dragtable(idragfile)
+
+      IMPLICIT NONE
+
+      INCLUDE 'idim'
+
+      INCLUDE 'COMMONS/radtran3'
+      INCLUDE 'COMMONS/xforce'
+      
+      INTEGER i, j, k
+      INTEGER idragfile
+      REAL hmass, hillmass
+
+      hmass = hillmass(planetmass, 0, xmass, 0)
+
+      WRITE (idragfile, ERR=100) idragresr, idragresp, idragrest
+      WRITE (idragfile, ERR=100) dragtrmin, dragtrmax, dragtdr, 
+     &     dragtpmin, dragtpmax, dragtdp, dragttmin, dragttmax, dragtdt
+      WRITE (idragfile, ERR=100) planetmass, hmass
+
+      do i = 1, idragresr
+         do j = 1, idragresp
+            do k = 1, idragrest
+               WRITE (idragfile, ERR=100) dragenergy(i,j,k)
+            enddo
+         enddo
+      enddo
+
+      CLOSE (idragfile)
+
+      RETURN
+      
+ 100  print *, 'An error has occurred writing drag file'
+      STOP
+
+      end subroutine write_dragtable
+

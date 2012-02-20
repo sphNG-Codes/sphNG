0c9089cfaaa4691ea7728f2ee76815165a20e413
Ben Ayliffe <ayliffe@astro.ex.ac.uk>
2012-02-20 17:44:18 +0000

    Calculates position of gas particle for spherical gas drag grid.
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1393 5d52cb8c-a192-40c0-a655-fe23f7e14f03

---
+++ sphNG/sphericalposition.f
@@ -0,0 +1,45 @@
+      subroutine sphericalpos(xi,yi,zi,radius,phi,theta)
+
+c************************************************************
+c                                                           *
+c  Routine to find a position in spherical coordinates      *
+c  about a planet, keeping the angles relative to the line  *
+c  connecting the star and planet.                          *
+c                                                           *
+c  Star-Planet line at +-pi on planet's surface.            *
+c                                                           *
+c  Ben Ayliffe, 17th November 2011.                         *
+c                                                           *
+c************************************************************
+
+      IMPLICIT NONE
+
+      INCLUDE 'COMMONS/physcon'
+      INCLUDE 'COMMONS/pxpy'
+      
+      REAL xi, yi, zi
+      REAL radius, phi, theta
+      REAL dx, dy, dz
+      REAL xr, yr, dxr, dyr
+      REAL radius_p, phi_p, theta_p
+
+      radius_p = sqrt(px*px + py*py + pz*pz)
+      phi_p = ATAN2(py,px)
+      theta_p = ACOS(pz/radius_p)
+
+      dx = xi - px
+      dy = yi - py
+      dz = zi - pz
+
+      radius = sqrt(dx*dx + dy*dy + dz*dz)
+
+      xr =  xi*cos(phi_p) + yi*sin(phi_p)
+      yr = -xi*sin(phi_p) + yi*cos(phi_p)
+      dxr = xr - radius_p
+      dyr = yr
+
+      phi = ATAN2(dyr,dxr)
+
+      theta = ACOS(zi/radius)
+
+      end subroutine sphericalpos

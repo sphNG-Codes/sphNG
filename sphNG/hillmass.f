bfb12fc21c341e0a8797d4de070649d7daa8d3a7
Ben Ayliffe <ayliffe@astro.ex.ac.uk>
2012-02-20 17:24:08 +0000

    Hillmass calculations now works with sinks and potentials of many sorts.
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1380 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/hillmass.f
+++ sphNG/hillmass.f
@@ -1,7 +1,10 @@
-      FUNCTION hillmass(planetmass, sswitch, xmass)
+      FUNCTION hillmass(planetmass, sswitch, xmass, isink)
 
-c--Added sswitch such that this calculation now works for sink
-c  particles as well as fixed potentials.
+c--Added sswitch such that this calculation now works for sink (1)
+c  particles as well as fixed potentials (0).
+
+c--Added isink, so it no longer assumes listpm(1), allowing multiple
+c  sink particles to be used.
 
       INCLUDE 'idim'
 
@@ -17,33 +20,50 @@ c  particles as well as fixed potentials.
       EQUIVALENCE (rr, tempsort)
 
       REAL rr(idim), hillr, hillm, hillmass, planetmass, xmass
-      INTEGER ilist(idim), sswitch
+      REAL rorbit, xsub, ysub, zsub
+      INTEGER ilist(idim), sswitch, isink
 
       hillm = planetmass
-      hillr = (hillm/(3.*xmass))**(1.0/3.0)
 
       IF (irotpot.EQ.1 .AND. (px.EQ.0.0 .AND. py.EQ.0.0)) THEN
          print *, 'px and py = 0 in externf.'
          STOP
       ENDIF
 
-      DO i = 1, npart
-         IF (sswitch.EQ.0) THEN
-            IF (irotpot.EQ.0) THEN
-               rr(i) = SQRT((xyzmh(1,i)-1.0)**2 + xyzmh(2,i)**2 +
-     &              xyzmh(3,i)**2)
-            ELSE
-               rr(i) = SQRT((xyzmh(1,i)-px)**2 + (xyzmh(2,i)-py)**2 +
-     &              xyzmh(3,i)**2)
-            ENDIF
-         ELSEIF (sswitch.EQ.1) THEN
-            rr(i) = SQRT((xyzmh(1,i)-xyzmh(1,listpm(1)))**2 +
-     &           (xyzmh(2,i) - xyzmh(2,listpm(1)))**2 +
-     &           (xyzmh(3,i) - xyzmh(3,listpm(1)))**2)
+      IF (sswitch.EQ.0) THEN
+         IF (irotpot.EQ.0) THEN
+            rorbit = 1.0
+            xsub = 1.0
+            ysub = 0.0
+            zsub = 0.0
+         ELSE
+            rorbit = sqrt(px**2 + py**2 + pz**2)
+            xsub = px
+            ysub = py
+            zsub = pz
          ENDIF
+      ELSEIF (sswitch.EQ.1) THEN
+         IF (isink.EQ.0) THEN
+            print *, 'ERROR: hillmass, isink = 0'
+            STOP
+         ENDIF
+         rorbit = sqrt(xyzmh(1,listpm(isink))**2 +
+     &        xyzmh(2,listpm(isink))**2 +
+     &        xyzmh(3,listpm(isink))**2)
+         xsub = xyzmh(1,listpm(isink))
+         ysub = xyzmh(2,listpm(isink))
+         zsub = xyzmh(3,listpm(isink))
+      ENDIF
+
+      DO i = 1, npart
+         rr(i) = SQRT((xyzmh(1,i) - xsub)**2 +
+     &                (xyzmh(2,i) - ysub)**2 +
+     &                (xyzmh(3,i) - zsub)**2)
       END DO
 
       CALL indexx2(npart, rr, ilist)
+
+      hillr = rorbit*(hillm/(3.*xmass))**(1.0/3.0)
 
       DO j = 1, npart
          i = ilist(j)

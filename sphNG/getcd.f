10762a2b2647d5b1b5550c921be49bc22b1fd4a5
Ben Ayliffe <ayliffe@astro.ex.ac.uk>
2012-02-20 17:34:51 +0000

    Temporary submission to keep the code compiling. Need to allow for more than one drag scheme to be stored in here.
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1385 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/getcd.f
+++ sphNG/getcd.f
@@ -1,54 +1,32 @@
-      REAL FUNCTION GETCD(rho_g, vr, cs)
+      REAL FUNCTION GETCD(rho_g, vr, cs, r_planetesimal)
 
 c************************************************************
 c                                                           *
 c  Routine to find drag coefficient for a planetesimal in   *
-c  gas, based upon Brasser et al. 2007.                     *
+c  gas, based upon Perets & Murray-Clay 2011                *
 c                                                           *
-c  Ben Ayliffe, 12th April 2011.                            *
+c  Ben Ayliffe, 8th July 2011.                              *
 c                                                           *
 c************************************************************
 
       IMPLICIT NONE
 
-      REAL rho_g
-      REAL r_p, vr, cs
-      REAL reynolds, knudsen, mach
-
-      INCLUDE 'idim'
-
-      INCLUDE 'COMMONS/units'
       INCLUDE 'COMMONS/physcon'
-      INCLUDE 'COMMONS/planetesimal'
-c
-c--Equation 19, Brasser et al. 2007
-c  Coefficient divided by 10 to convert from Brasser's SI units.
-c
-      knudsen = (1.67E-9*udist**2/umass)/(rho_g*r_planetesimal)
-c
-c--Equation 14, Brasser et al. 2007
-c
-      mach = vr/cs
-c
-c--Equation 18, Brasser et al. 2007
-c  Using gamma = 5/3, rather than the 7/5 assumed by Brasser et al.
-c
-      reynolds = 4.85*mach/knudsen
-c
-c--Set CD depending upon regime defined by above quantities.
-c
-      IF (mach.GE.1.0) THEN
-         getcd = 2.0
-      ELSEIF (mach.LT.1.0 .AND. reynolds.GE.1.0E3) THEN
-         getcd = 0.44 + 1.56*mach**2
-      ELSEIF (reynolds.LT.1.0E3) THEN
-         getcd = 2.*mach**2 + (1. - mach**2)*(24.*(1. +
-     &        0.15*reynolds**0.687)/reynolds)
-      ELSEIF (knudsen.GE.1.0) THEN
-         getcd = 0.0
-c--If knudsen > 1, Brasser's formulation gives rho_g = 0.
-c  This means the gas drag should be zero, hence CD = 0.
-      ENDIF
+      INCLUDE 'COMMONS/astrcon'
+
+      REAL rho_g, vr, cs
+      REAL reynolds, lambda_mfp
+      REAL vthermal, r_planetesimal
+      
+c      lambda_mfp = 1.619E-15/rho_g
+c--Constant using lambda as defined in Perets & Murray-Clay 2011
+      lambda_mfp = 6.0E-15/rho_g
+      vthermal = sqrt(8./pi)*cs
+      reynolds = 2.*r_planetesimal*vr/(0.5*vthermal*lambda_mfp)
+         
+      getcd = (24./reynolds)*(1.+0.27*reynolds)**0.43 + 
+     &     0.47*(1.-exp(-0.04*reynolds**0.38))
 
       RETURN
+
       END FUNCTION GETCD

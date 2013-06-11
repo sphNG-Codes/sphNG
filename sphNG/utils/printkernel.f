66239637504691da098739843e28777d7b24d37d
Daniel Price <daniel.price@monash.edu>
2013-06-11 10:08:22 +0000

    added printkernel utility for printing kernel tables; also kernel selection done better in Makefile
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1628 5d52cb8c-a192-40c0-a655-fe23f7e14f03

---
+++ sphNG/utils/printkernel.f
@@ -0,0 +1,27 @@
+      PROGRAM printkernel
+      IMPLICIT NONE
+      
+      INCLUDE '../COMMONS/kerne'
+      INCLUDE '../COMMONS/table'
+      INTEGER i
+      REAL vi, phi
+      
+      CALL constan
+      CALL ktable
+      
+      WRITE(*,*) '# sphNG kernel table, radkern = ',radkernel, 
+     &           ' cnormk = ',cnormk
+      DO i=0,itable
+         vi = sqrt(i*dvtable)
+         phi = fpoten(i)
+         IF (vi.GT.part2kernel) THEN
+            phi = phi + part2potenkernel/vi
+         ELSEIF (vi.GT.part1kernel) THEN
+            phi = phi + part1potenkernel/vi
+         ENDIF
+         WRITE(*,"(6(es12.4,1x))") vi,
+     &        cnormk*wij(i),cnormk*grwij(i),fmass(i),phi,
+     &        dphidh(i)
+      ENDDO
+      
+      END

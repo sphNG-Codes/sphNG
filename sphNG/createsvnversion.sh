6156c9a9f08e3a6b2f3e0526fecb5f80dd2a4167
Ben Lewis <blewis@astro.ex.ac.uk>
2014-07-02 06:15:59 +0000

    Replace createsvnversion.csh with createsvnversion.sh which uses sh (or bash) not the C shell.  Bash in in POSIX and therefore guaranteed(ish) to be availble, e.g. on Post-Zen, whilst tcsh is less so
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1664 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/createsvnversion.sh
+++ sphNG/createsvnversion.sh
@@ -1,12 +1,14 @@
-#!/bin/csh
+#!/bin/sh
+
+echo '      CHARACTER*20 svnversion' > svn_version.h
 
 # Check whether subversion is available
-which svnversion > /dev/null
+which svnversion 2>&1 > /dev/null
 
 # Set the version number using svnversion if available or set to unknown otherwise
-echo '      CHARACTER*20 svnversion' >! svn_version.h
-if ($? == 0) then 
+if [ $? -eq 0 ] ; then
     echo '      PARAMETER (svnversion = "-'`svnversion`'")' >> svn_version.h
 else
     echo '      PARAMETER (svnversion = "-'unknown'")' >> svn_version.h
-endif
+fi
+

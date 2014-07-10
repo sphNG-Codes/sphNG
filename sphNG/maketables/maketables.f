554e3ea372cb57ea18d3d31dfbbd6c99045678b7
Matthew Bate <M.R.Bate@exeter.ac.uk>
2014-07-10 06:55:02 +0000

    Fixed the calculation of the tables of 1/mu.  Earlier version tabulated mu rather than 1/mu (due to changes made by Daniel Price from 2011), and there was no way to generate 1/mu(rho,u) before this update.  Files affected: maketables.f, invert.f
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1670 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/maketables/maketables.f
+++ sphNG/maketables/maketables.f
@@ -1,9 +1,22 @@
 c-------------------------------------------------------------------------
-c This is the driver program for creating the specific heat and mean
-c molecular weight tables. Written by Stuart Whitehouse
 c
-c Was previously part of specificheat.f, program part now in separate 
-c file so subroutines can be called from other programs (DJP 23/5/11)
+c--This is the driver program for creating the specific heat and mean
+c     molecular weight tables. Originally written by Stuart Whitehouse
+c
+c--Was previously part of specificheat.f, program part now in separate 
+c     file so subroutines can be called from other programs (DJP 23/5/11)
+c
+c--This program makes a table called "specheattbl", which gives the ratio
+c     of the internal energy to the temperature of the gas, which would
+c     be c_v = U/T if c_v was a constant.  However, the table is a function
+c     of density and temperature.  The code sphNG does not evolve temperature,
+c     it evolves specific internal energy, u.  So the code actually wants 
+c     quantities as functions of  density and u.  These tables are made
+c     by running "invert.f" after the "specheattbl" has been created.
+c     This program inverts the table to give U/T as a function of density
+c     and u, and produces a table of u  as a function of density and T.
+c     It also creates a table of 1/mu, where mu is the mean particle mass 
+c     which is also indexed using density and u.
 c
 c-------------------------------------------------------------------------
       PROGRAM MAKETABLE
@@ -23,12 +36,9 @@ c-------------------------------------------------------------------------
 
       Rg=8.3145d7
 
-!     OPEN(UNIT=55,FILE='specheattbl')
-
       OPEN(UNIT=8,FILE='specheattbl',FORM='unformatted')
-!      OPEN(UNIT=10,FILE='molmasstbl',FORM='unformatted')
 
-      DO nrho=-30000,3000,5! log10(density) -20,3,0.005
+      DO nrho=-30000,3000,5! log10(density) -30,3,0.005
 
          IF(MOD(nrho,500).EQ.0) PRINT *,nrho
          lrho=nrho/1000.0
@@ -43,10 +53,8 @@ c-------------------------------------------------------------------------
 c            write (95,99001) tm,specific/Rg,uoverT/Rg,mu
 99001       FORMAT(4(1PE12.5,1X))
             cvv(I,J)=log10(uoverT)
-            muu(I,J)=log10(mu)
+            muu(I,J)=log10(1./mu)
          END DO
-
-c         STOP
 
          IF(MOD(nrho,500).EQ.0) PRINT *,cvv(I,900)
 
@@ -57,13 +65,11 @@ c         STOP
 
       DO j=1,umxt
          WRITE(8) (cvv(i,j), i=1, umxrh)
-!         WRITE(10) (muu(i,j), i=1, umxrh)
          PRINT *,cvv(2096,j),muu(2096,j)
-c         WRITE(66,*) cvv(4600,j)
       END DO
-!      PRINT *,cvv(20,50)
+      CLOSE(8)
+
       PRINT *,"Complete"
-      CLOSE(55)
 
       END 
             

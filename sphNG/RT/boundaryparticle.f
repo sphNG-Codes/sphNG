f0aa3cb172bfefdd6d1705c8a2f8595087ee0c3e
Matthew Bate <M.R.Bate@exeter.ac.uk>
2016-05-09 17:35:01 +0000

    Altered RT/boundaryparticle.f so that when idustRT=1 boundary particles for radiative transfer cannot be set using bounddens
    git-svn-id: https://repository.astro.ex.ac.uk/sphng/trunk@1927 5d52cb8c-a192-40c0-a655-fe23f7e14f03

--- sphNG/RT/boundaryparticle.f
+++ sphNG/RT/boundaryparticle.f
@@ -27,35 +27,37 @@ c
 c
 c--Disc boundary
 c
-      ELSEIF (ibound.EQ.100) THEN
-         boundtest = ABS(xyzmh(3,i)/(hoverr*SQRT(
-     &        xyzmh(1,i)**2 + xyzmh(2,i)**2)))
-         boundaryparticle = boundtest.GT.bounddens
+      ELSEIF (idustRT.EQ.0) THEN
+         IF (ibound.EQ.100) THEN
+            boundtest = ABS(xyzmh(3,i)/(hoverr*SQRT(
+     &           xyzmh(1,i)**2 + xyzmh(2,i)**2)))
+            boundaryparticle = boundtest.GT.bounddens
 c
 c--Disc + planet boundary
 c
-      ELSEIF (ibound/10.EQ.10) THEN
-         radu = sqrt(xyzmh(1,i)**2 + xyzmh(2,i)**2)
-         izoverh = INT((radu - rmind)/deltar + 1)
-         IF (izoverh.LT.1) izoverh = 1
-         IF (izoverh.GT.1000) izoverh = 1000
-         mu = (radu - (rmind + (izoverh-1.0)*deltar))/deltar
-         bounddenslocal = (zoverh(izoverh+1) -
-     &        zoverh(izoverh))*mu + zoverh(izoverh)
-         IF (use_tprof) THEN
-            boundtest = ABS(xyzmh(3,i)/(hoverr*radu**
-     &           (0.5*(tprof+3))))
-         ELSE
-            boundtest = ABS(xyzmh(3,i)/(hoverr*radu))
-         ENDIF
-         boundaryparticle = boundtest.GT.bounddenslocal
-         IF (radu.LT.0.385 .AND. use_tprof)
-     &        boundaryparticle = .TRUE.
+         ELSEIF (ibound/10.EQ.10) THEN
+            radu = sqrt(xyzmh(1,i)**2 + xyzmh(2,i)**2)
+            izoverh = INT((radu - rmind)/deltar + 1)
+            IF (izoverh.LT.1) izoverh = 1
+            IF (izoverh.GT.1000) izoverh = 1000
+            mu = (radu - (rmind + (izoverh-1.0)*deltar))/deltar
+            bounddenslocal = (zoverh(izoverh+1) -
+     &           zoverh(izoverh))*mu + zoverh(izoverh)
+            IF (use_tprof) THEN
+               boundtest = ABS(xyzmh(3,i)/(hoverr*radu**
+     &              (0.5*(tprof+3))))
+            ELSE
+               boundtest = ABS(xyzmh(3,i)/(hoverr*radu))
+            ENDIF
+            boundaryparticle = boundtest.GT.bounddenslocal
+            IF (radu.LT.0.385 .AND. use_tprof)
+     &           boundaryparticle = .TRUE.
 c
 c--Low density gas boundary (e.g. for stellar cluster formation)
 c
-      ELSE
-         boundaryparticle = rhoi.LT.bounddens
+         ELSE
+            boundaryparticle = rhoi.LT.bounddens
+         ENDIF
       ENDIF
 
       RETURN

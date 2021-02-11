0777a51c0ec31f6440a4329bb6b502b909922688
Matthew Bate <M.R.Bate@exeter.ac.uk>
2021-02-11 12:47:28 +0000

    Massive Star Feedback: Added subroutines to build stellar    photoionisation tables for ionising feedback from stars.
    Modified: COMMONS/stellarradiation build_stellar_radiation_table.f
       preset.F

--- sphNG/build_stellar_radiation_table.f
+++ sphNG/build_stellar_radiation_table.f
@@ -61,3 +61,146 @@ c      STOP
       END
 
 c-----------------------------------------------------------
+
+      SUBROUTINE build_photoionisation_heating_rate_table
+c
+c--Heating rate due to hydrogen ionisation by UV photons.
+c     Heating rate equation from, e.g. Kannan et al. (2019), eqn. 57
+c     Cross-section is from Verner et al. (1996)
+c     Depends on stellar spectrum, so tabulate based on stellar
+c     blackbody temperature, from T=1000 to ~55,000 K.
+c
+c--NOTE: The values are in cgs units, not code units.
+c
+      IMPLICIT REAL*8 (A-H,O-Z)
+
+      INCLUDE 'idim'
+      INCLUDE 'igrape'
+
+      INCLUDE 'COMMONS/physcon'
+      INCLUDE 'COMMONS/stellarradiation'
+
+      xSTARfac = EXP(xSTARlogfac)
+      dlogv = LOG(1.1)
+
+      DO k = 1, nstellar_T
+         T = 1000.0 * xSTARfac**(k-1)
+
+         x_integral = 0.
+         x_integral_norm = 0.
+         x_integral_2 = 0.
+         x_integral_ionisingflux = 0.
+         x_integral_N_ionphotons = 0.
+c
+c--Start from 13.6 eV photons (v=13.6 eV / h)
+c
+         v_ion = 13.6 * eleccharge / planckconst
+         v = v_ion
+         DO i = 1, 400
+            xJ = planck(v,T)
+            IF (xJ.GT.1e-40 .AND. v.GT.0.1) THEN
+               sigma = hydrogen_cross_section(v)
+
+               energyfac = planckconst*(v - v_ion)
+               x_integral = x_integral + xJ*sigma*energyfac*v*dlogv
+
+               x_integral_2 = x_integral_2 + 
+     &              xJ*sigma*v_ion*planckconst*v*dlogv
+               x_integral_norm = x_integral_norm + xJ*sigma*v*dlogv
+
+               x_integral_ionisingflux = x_integral_ionisingflux +
+     &              xJ*v*dlogv
+               x_integral_N_ionphotons = x_integral_N_ionphotons +
+     &              xJ*v*dlogv/(planckconst*v)
+
+c               IF (k.EQ.88) WRITE (41,33001) v,sigma,!energyfac,
+c     &              xJ*sigma*energyfac*v*dlogv,
+c     &              xJ*sigma*v_ion*planckconst*v*dlogv
+c     &              xJ*v*dlogv,planckconst*v*xJ*v*dlogv
+
+            ENDIF
+            v = v * 1.1
+         END DO
+
+         photoionisation_heating_T_table(k) = x_integral * pi
+
+         IF (x_integral_norm.EQ.0.) x_integral_norm = pi
+
+c         WRITE (90,33001) T,photoionisation_heating_T_table(k),
+c     &        x_integral_2/x_integral_norm, 
+c     &        photoionisation_heating_T_table(k)/x_integral_norm,
+c     &        x_integral_N_ionphotons *pi
+c     &        * 4.0*pi*(7.0 * 7.0E+10)**2
+33001    FORMAT(5(1PE12.5,1X))
+
+      END DO
+
+      RETURN
+      END
+
+c-----------------------------------------------------------
+
+      FUNCTION hydrogen_cross_section(v)
+c
+c--Hydrogen photoionisation cross-section is from Verner et al. (1996)
+c
+c     Units are cm^2 (i.e. cm^2/H)
+c
+c--NOTE: The values are in cgs units, not code units.
+c
+      IMPLICIT NONE
+
+      REAL*8, PARAMETER :: P = 2.963D0
+      REAL*8, PARAMETER :: ya = 3.288D1
+
+      REAL*8 hydrogen_cross_section,v,E,x,y
+
+      INCLUDE 'COMMONS/physcon'
+
+      E = planckconst * v / eleccharge
+
+      x = E/0.4298
+      y = x
+      hydrogen_cross_section = 5.475E-14 * (x - 1.0)**2 * 
+     &     y**(0.5*P-5.5) * (1.0 + SQRT(y/ya))**(-P)
+
+      RETURN
+      END
+
+c-----------------------------------------------------------
+
+      FUNCTION photoionisation_heating(Tstar)
+c
+c--Returns the photoionisation heating rate for a star with surface
+c     temperature Tstar.
+c     The heating rate is in erg/s per H.  But need to multiply
+c     by the ratio of (Rstar/distance)^2 to get
+c     the local heating rate in units of erg/s per H.
+c
+      IMPLICIT NONE
+
+      INCLUDE 'idim'
+      INCLUDE 'igrape'
+
+      INCLUDE 'COMMONS/physcon'
+      INCLUDE 'COMMONS/stellarradiation'
+
+      REAL*8 photoionisation_heating,Tstar,xtemperature
+      INTEGER itemperature
+
+      IF (Tstar.LE.3000.) THEN
+         photoionisation_heating = 0.
+      ELSE
+         xtemperature = LOG(Tstar/1000.0)/xSTARlogfac + 1.5
+         itemperature = MIN(nstellar_T-2, INT(xtemperature)) + 1
+         IF (xtemperature.GT.nstellar_T) xtemperature = nstellar_T
+         
+         photoionisation_heating = 
+     &        photoionisation_heating_T_table(itemperature) + 
+     &        (photoionisation_heating_T_table(itemperature + 1) - 
+     &        photoionisation_heating_T_table(itemperature)) *
+     &        (xtemperature - itemperature + 1)
+      ENDIF
+      
+      RETURN
+      END
